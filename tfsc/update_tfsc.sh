eafc251d0dfbb34d58f6fa6f4b39cb1961ab7cbf
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-08-25 13:40:24 +0300

    upd

--- tfsc/update_tfsc.sh
+++ tfsc/update_tfsc.sh
@@ -7,7 +7,7 @@ rm -rf $HOME/tfsc/config.json
 # rm -rf $HOME/tfsc/data.db
 
 cd $HOME/tfsc/
-wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.2.0_681c2ec_devnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.3.0_d6eda1a_devnet
 
 cd $HOME/tfsc/
 PUB_IP=$(wget -qO- eth0.me);wget -qO- pastebin.com/raw/MfS126mf|sed 's#\"ip\": \"pub_ip\"#\"ip\": '\"${PUB_IP}\"'#' > config.json
