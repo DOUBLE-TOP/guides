c19838e25725e1ae9e4986c47f971382dea27fad
danzilla0x <8739617+danzilla0x@users.noreply.github.com>
2022-09-04 18:46:51 +0300

    Update gear script to support non-root users

--- Gear/update_gear.sh
+++ Gear/update_gear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://builds.gear.rs/gear-nightly-linux-x86_64.tar.xz
-sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
+tar -xvf gear-nightly-linux-x86_64.tar.xz -C $HOME
 rm gear-nightly-linux-x86_64.tar.xz
 $HOME/gear-node purge-chain -y
 sudo systemctl restart gear
