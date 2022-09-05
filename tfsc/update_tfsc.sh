23b0f72e1c9c7080b3a86af388e2daaafae04526
razumv <razumv95@gmail.com>
2022-09-05 17:52:38 +0200

    fix update\delete config

--- tfsc/update_tfsc.sh
+++ tfsc/update_tfsc.sh
@@ -3,15 +3,15 @@
 tmux kill-session -t tfsc
 
 rm -rf $HOME/tfsc/tfsc
-# rm -rf $HOME/tfsc/config.json
+rm -rf $HOME/tfsc/config.json
 # rm -rf $HOME/tfsc/data.db
 
 cd $HOME/tfsc/
 wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.4.0_762f9d0_devnet
 
 cd $HOME/tfsc/
-# PUB_IP=$(wget -qO- eth0.me);wget -qO- pastebin.com/raw/MfS126mf|sed 's#\"ip\": \"pub_ip\"#\"ip\": '\"${PUB_IP}\"'#' > config.json
-# sed -i "s/OFF/INFO/" "$HOME/tfsc/config.json"
+PUB_IP=$(wget -qO- eth0.me);wget -qO- pastebin.com/raw/MfS126mf|sed 's#\"ip\": \"pub_ip\"#\"ip\": '\"${PUB_IP}\"'#' > config.json
+sed -i "s/OFF/INFO/" "$HOME/tfsc/config.json"
 
 chmod +x $HOME/tfsc/tfsc
 
