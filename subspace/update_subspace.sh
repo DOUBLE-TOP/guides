a9d16a12595424c6215c323c36bb36bb288c66ab
razumv <razumv95@gmail.com>
2022-09-06 15:35:15 +0200

    update to gemini-2a-2022-sep-06

--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -16,7 +16,7 @@ function line {
 
 function get_vars {
   export CHAIN="gemini-1"
-  export RELEASE="gemini-1b-2022-jun-18"
+  export RELEASE="gemini-2a-2022-sep-06"
   export SUBSPACE_NODENAME=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-name" | awk -F\" '{print $4}')
   export WALLET_ADDRESS=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-reward-address" | awk -F\" '{print $4}')
   export PLOT_SIZE=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-plot-size" | awk -F\" '{print $4}')
