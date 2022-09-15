1d88366df31789faf3e72184c283e607e8c5a11f
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-09-15 17:31:19 +0300

    гзв

--- massa/clear_massa.sh
+++ massa/clear_massa.sh
@@ -20,7 +20,7 @@ function delete {
 }
 
 function install {
-  wget https://github.com/massalabs/massa/releases/download/TEST.14.0/massa_TEST.14.0_release_linux.tar.gz
+  wget https://github.com/massalabs/massa/releases/download/TEST.14.0/massa_TEST.14.1_release_linux.tar.gz
   tar zxvf massa_TEST.14.0_release_linux.tar.gz -C $HOME/
 }
 
