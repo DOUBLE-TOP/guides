d6ae0ba7eaf8a802af4034534c75500c0131d344
razumv <razumv95@gmail.com>
2022-09-15 11:38:01 +0200

    small fix

--- bazuka/update.sh
+++ bazuka/update.sh
@@ -13,6 +13,7 @@ git pull origin master
 cargo build
 cargo install --path .
 rm -f /usr/local/bin/bazuka
+rm -rf $HOME/.bazuka-debug
 sudo mv $HOME/bazuka/target/debug/bazuka /usr/local/bin/
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/bazuka.service
@@ -22,7 +23,7 @@ After=network.target
 
 [Service]
 User=$USER
-ExecStart=`RUST_LOG=info which bazuka` node --listen 0.0.0.0:8765 --external $(wget -qO- eth0.me):8765 --network debug --db ~/.bazuka-debug --bootstrap 152.228.155.120:8765 --bootstrap 5.161.152.123:8765 --bootstrap 65.108.201.41:8765 --bootstrap 185.213.25.229:8765 --bootstrap 45.88.106.199:8765 --bootstrap 148.251.1.124:8765 --bootstrap 195.54.41.115:8765 --bootstrap 195.54.41.130:8765
+ExecStart=`RUST_LOG=info which bazuka` node --listen 0.0.0.0:8765 --external $(wget -qO- eth0.me):8765 --network debug --db $HOME/.bazuka-debug --bootstrap 152.228.155.120:8765 --bootstrap 5.161.152.123:8765 --bootstrap 65.108.201.41:8765 --bootstrap 185.213.25.229:8765 --bootstrap 45.88.106.199:8765 --bootstrap 148.251.1.124:8765 --bootstrap 195.54.41.115:8765 --bootstrap 195.54.41.130:8765
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
