8540a3b277611230a8c78dd8f8971751a118a82f
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-09-22 13:14:39 +0300

    upd

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -11,7 +11,7 @@ cd $HOME/massa/massa-client
 massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-        balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Final balance" | awk '{ print $3 }')
+        balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Sequential balance" | awk '{ print $4 }')
         int_balance=${balance%%.*}
         if [ $int_balance -gt "99" ]; then
                 echo "More than 99"
