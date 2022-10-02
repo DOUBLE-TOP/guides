af4dbdb1e0087436909261f4f12800480d2e33fd
razumv <razumv95@gmail.com>
2022-10-03 01:59:21 +0200

    fix

--- graph/indexer.sh
+++ graph/indexer.sh
@@ -6,4 +6,5 @@ mkdir -p $HOME/graph-indexer
 wget -O $HOME/graph-indexer/docker-compose.yml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/docker-compose.yml
 wget -O $HOME/graph-indexer/prometheus.yml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/prometheus.yml
 
-docker-compose -f $HOME/graph-indexer/prometheus.yml up -d
+cd $HOME/graph-indexer/
+docker-compose up -d
