460162c595d0c7ecd6e8a15ff2413244c2dc8a08
razumv <razumv95@gmail.com>
2022-10-15 10:07:23 +0200

    fix

--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -109,7 +109,7 @@ function update_subspace {
   docker-compose down
   # docker volume rm subspace_docker_subspace-farmer subspace_docker_subspace-node
   # docker volume rm subspace_docker_farmer-data subspace_docker_node-data
-  docker volume rm subspace_docker_farmer-data
+  # docker volume rm subspace_docker_farmer-data
   eof_docker_compose
   docker-compose pull
   docker-compose up -d
