b4b0b34bcca6b7647da3e2f3f07dfb1a2e59a854
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-10-21 13:45:51 +0300

    fix

--- bazuka/update.sh
+++ bazuka/update.sh
@@ -34,7 +34,7 @@ After=network.target
 
 [Service]
 User=$USER
-ExecStart=`RUST_LOG=info which bazuka` node --listen 0.0.0.0:8765 --external $(wget -qO- eth0.me):8765 --network debug --db $HOME/.bazuka-debug --bootstrap 152.228.155.120:8765 --bootstrap 5.161.152.123:8765 --bootstrap 65.108.201.41:8765 --bootstrap 185.213.25.229:8765 --bootstrap 45.88.106.199:8765 --bootstrap 148.251.1.124:8765 --bootstrap 195.54.41.115:8765 --bootstrap 195.54.41.130:8765 --discord-handle "$BAZUKA_DISCORD"
+ExecStart=`RUST_LOG=info which bazuka` node --listen 0.0.0.0:8765 --external $(wget -qO- eth0.me):8765 --network chaos --db $HOME/.bazuka-debug --bootstrap 152.228.155.120:8765 --bootstrap 5.161.152.123:8765 --bootstrap 65.108.201.41:8765 --bootstrap 185.213.25.229:8765 --bootstrap 45.88.106.199:8765 --bootstrap 148.251.1.124:8765 --bootstrap 195.54.41.115:8765 --bootstrap 195.54.41.130:8765 --discord-handle "$BAZUKA_DISCORD"
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
