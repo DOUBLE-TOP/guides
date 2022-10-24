8e95a6a6b9b01ebd942751d9daf2b1d670e3a6da
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-10-24 13:02:56 +0300

    upd

--- Gear/update_gear.sh
+++ Gear/update_gear.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
 
 sudo systemctl stop gear
 /root/gear-node purge-chain -y
+
+echo "-----------------------------------------------------------------------------"
+echo "Выполняем Обновление"
+echo "-----------------------------------------------------------------------------"
+
 
 wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz
 sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
 rm gear-nightly-linux-x86_64.tar.xz
 
+sudo tee <<EOF >/dev/null /etc/systemd/system/gear.service
+[Unit]
+Description=Gear Node
+After=network.target
+
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME
+ExecStart=$HOME/gear \
+        --name $NODENAME_GEAR \
+        --execution wasm \
+	--port 31333 \
+        --telemetry-url 'ws://telemetry-backend-shard.gear-tech.io:32001/submit 0' \
+	--telemetry-url 'wss://telemetry.postcapitalist.io/submit 0'
+Restart=always
+RestartSec=10
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl restart systemd-journald &>/dev/null
+sudo systemctl daemon-reload &>/dev/null
 sudo systemctl restart gear
+echo "-----------------------------------------------------------------------------"
 echo "Обновление завершено успешно"
+echo "-----------------------------------------------------------------------------"
