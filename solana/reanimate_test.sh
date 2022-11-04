da8496d4d7cfe975cfc8cd7be8b6fec25dd740f1
razumv <razumv95@gmail.com>
2022-11-04 15:22:51 +0100

    add reanimate solana 04.11.22

---
+++ solana/reanimate_test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+solana-install init v1.14.7
+
+sudo systemctl stop solana
+
+sudo rm -rf $HOME/ledger/*
+
+# wget -O $HOME/ledger/snapshot.tar.bz2 --trust-server-names http://194.126.172.246:8899/snapshot-160991176-34QhWFp6afjFWmviBqaAiLbk62w75r18UB6qYqfZ7Wjt.tar.zst
+
+bash -c "cat > $HOME/solana.service<<EOF
+[Unit]
+Description=Solana TdS node
+After=network.target syslog.target
+StartLimitIntervalSec=0
+[Service]
+User=$USER
+Type=simple
+Restart=always
+RestartSec=1
+LimitNOFILE=1024000
+Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=tds,u=testnet_write,p=c4fa841aa918bf8274e3e2a44d77568d9861b3ea"
+ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
+--wait-for-supermajority 160991176 \
+--expected-shred-version 8135 \
+--entrypoint entrypoint.testnet.solana.sergo.dev:8001 \
+--entrypoint entrypoint.testnet.solana.com:8001 \
+--entrypoint entrypoint2.testnet.solana.com:8001 \
+--entrypoint entrypoint3.testnet.solana.com:8001 \
+--expected-bank-hash XvCZcHax1PJnfNj5TnzkqU6ksqU4tZUpcwYGi7fbKGi \
+--expected-genesis-hash 4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY \
+--known-validator eoKpUABi59aT4rR9HGS3LcMecfut9x7zJyodWWP43YQ \
+--known-validator FnpP7TK6F2hZFVnqSUJagZefwRJ4fmnb1StS1NokpLZM \
+--known-validator 9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv \
+--known-validator 4rVaXrd7BLSFZMSm4Lq63nxkVyezGxsQVpUhc9LqbxVk \
+--wal-recovery-mode skip_any_corrupted_record \
+--identity $HOME/validator-keypair.json \
+--vote-account $HOME/vote-account-keypair.json \
+--ledger $HOME/ledger \
+--limit-ledger-size 50000000 \
+--dynamic-port-range 8000-8020 \
+--log $HOME/solana.log \
+--snapshot-interval-slots 500 \
+--maximum-local-snapshot-age 500 \
+--snapshot-compression none \
+--no-port-check \
+--rpc-bind-address 127.0.0.1 \
+--rpc-port 8899 \
+--accounts-db-caching-enabled \
+--full-rpc-api --no-snapshot-fetch
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+sudo systemctl daemon-reload
+
+source $HOME/solana-snapshot-finder/venv/bin/activate
+
+python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger -r https://api.testnet.solana.com
+
+sudo systemctl restart solana
