49711b3aea856c3e0f7cbadfd58b845322f3b5f1
razumv <razumv95@gmail.com>
2022-11-05 13:14:43 +0100

    fix 3

--- solana/reanimate_test.sh
+++ solana/reanimate_test.sh
@@ -21,7 +21,9 @@ Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=tds,u
 ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
 --wait-for-supermajority 160991176 \
 --expected-shred-version 4711 \
---hard-fork \
+--no-genesis-fetch
+--no-snapshot-fetch
+--entrypoint tsolv.im-0.net:8000 \
 --entrypoint entrypoint.testnet.solana.sergo.dev:8001 \
 --entrypoint entrypoint.testnet.solana.com:8001 \
 --entrypoint entrypoint2.testnet.solana.com:8001 \
@@ -46,7 +48,7 @@ ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator 
 --rpc-bind-address 127.0.0.1 \
 --rpc-port 8899 \
 --accounts-db-caching-enabled \
---full-rpc-api --no-snapshot-fetch
+--full-rpc-api
 ExecReload=/bin/kill -s HUP $MAINPID
 ExecStop=/bin/kill -s QUIT $MAINPID
 [Install]
@@ -55,8 +57,12 @@ EOF"
 
 sudo systemctl daemon-reload
 
-source $HOME/solana-snapshot-finder/venv/bin/activate
+# source $HOME/solana-snapshot-finder/venv/bin/activate
+#
+# python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger -r https://api.testnet.solana.com
 
-python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger -r https://api.testnet.solana.com
+wget -O $HOME/ledger/snapshot-160991176-5GMY4MtGzDnXYq9AVssRBxVyRj6YVRMjxfjZtqVBeP97.tar.zst http://tsolv.im-0.net:13899/snapshot-160991176-5GMY4MtGzDnXYq9AVssRBxVyRj6YVRMjxfjZtqVBeP97.tar.zst
+
+wget -O $HOME/ledger/genesis.tar.bz2 http://api.testnet.solana.com/genesis.tar.bz2
 
 sudo systemctl restart solana
