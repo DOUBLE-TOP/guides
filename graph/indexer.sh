4b35b61d2909e922752568bb6e14187d5c694889
razumv <razumv95@gmail.com>
2022-11-06 16:31:10 +0100

    small fix

--- graph/indexer.sh
+++ graph/indexer.sh
@@ -2,9 +2,18 @@
 
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 
-mkdir -p $HOME/graph-indexer
+mkdir -p $HOME/graph-indexer/{cli/scripts,configs}
 wget -O $HOME/graph-indexer/docker-compose.yml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/docker-compose.yml
 wget -O $HOME/graph-indexer/prometheus.yml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/prometheus.yml
+wget -O $HOME/graph-indexer/shell https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/shell
+wget -O $HOME/graph-indexer/configs/index_node.toml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/configs/index_node.toml
+wget -O $HOME/graph-indexer/configs/index_node_1.toml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/configs/index_node_1.toml
+wget -O $HOME/graph-indexer/configs/query_node.toml https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/configs/query_node.toml
+wget -O $HOME/graph-indexer/cli/Dockerfile https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/cli/Dockerfile
+wget -O $HOME/graph-indexer/cli/altDockerfile https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/cli/altDockerfile
+wget -O $HOME/graph-indexer/cli/alt2Dockerfile https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/cli/alt2Dockerfile
+wget -O $HOME/graph-indexer/cli/scripts/poi https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/graph/cli/scripts/poi
+chmod +x $HOME/graph-indexer/{shell,cli/scripts/poi}
 
 cd $HOME/graph-indexer/
 docker-compose up -d
