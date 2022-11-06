d750d68b82ef0f6d9cc340c8d066aa10583321ae
razumv <razumv95@gmail.com>
2022-11-06 13:23:54 +0100

    update tfsc 10

--- tfsc/tfsc.sh
+++ tfsc/tfsc.sh
@@ -26,7 +26,7 @@ echo -e "${GREEN}Скачиваем tfsc${NORMAL}"
 line
 mkdir -p $HOME/tfsc
 cd $HOME/tfsc/
-wget -O $HOME/tfsc/tfsc  https://uscloudmedia.s3.us-west-2.amazonaws.com/transformers/tfs_v0.9.1_ec15bf3_devnet
+wget -O $HOME/tfsc/tfsc  https://uscloudmedia.s3.us-west-2.amazonaws.com/transformers/test/tfs_v0.10.0_f62a29a_devnet
 chmod +x $HOME/tfsc/tfsc
 line
 echo -e "${GREEN}Конфигурируем и запускаем tfsc${NORMAL}"
