81d1208f3dc728f2dc6b2fb0e9ba373182303a7a
razumv <razumv95@gmail.com>
2022-11-17 10:29:45 +0100

    fix

--- solana/reanimate_test.sh
+++ solana/reanimate_test.sh
@@ -19,21 +19,23 @@ RestartSec=1
 LimitNOFILE=1024000
 Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=tds,u=testnet_write,p=c4fa841aa918bf8274e3e2a44d77568d9861b3ea"
 ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
+--wait-for-supermajority 161660256 \
+--expected-shred-version 6995 \
 --entrypoint entrypoint.testnet.solana.sergo.dev:8001 \
 --entrypoint tsolv.im-0.net:8000 \
 --entrypoint entrypoint.testnet.solana.com:8001 \
 --entrypoint entrypoint2.testnet.solana.com:8001 \
 --entrypoint entrypoint3.testnet.solana.com:8001 \
 --expected-genesis-hash 4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY \
+--expected-bank-hash GjU2DYkVVcd4LD3Yew1xSL8XibvVnhVse2U6b5JdNDhN \
 --known-validator eoKpUABi59aT4rR9HGS3LcMecfut9x7zJyodWWP43YQ \
---known-validator 9v7E6oEm1V86hjTubtBon7cRYPvQriWZKHZEX6j92Po4 \
+--known-validator 5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on \
+--known-validator td2GGWDsCJ6LvjN89oLJvmrDwE14neNrbqQ9s3tVkPy \
+--known-validator td3n5NGhP7JKWrL638gzau3NY7mF4K3ztZww3GkpywJ  \
 --known-validator FnpP7TK6F2hZFVnqSUJagZefwRJ4fmnb1StS1NokpLZM \
---known-validator J7v9ndmcoBuo9to2MnHegLnBkC9x3SAVbQBJo5MMJrN1 \
---wait-for-supermajority 160991176 \
+--known-validator BFquPCAYdjN9QyLVfuGrQdJTF9Ct7Z85FDxhFeLcpFqR \
 --no-snapshot-fetch \
 --no-genesis-fetch \
---expected-bank-hash GfNNxK4wS51NDWos2DQoLKU2ECiMEbFMPRw7bpDi9BoY \
---expected-shred-version 4711 \
 --only-known-rpc \
 --wal-recovery-mode skip_any_corrupted_record \
 --identity $HOME/validator-keypair.json \
@@ -62,8 +64,8 @@ sudo systemctl daemon-reload
 #
 # python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger -r https://api.testnet.solana.com
 
-wget -O $HOME/ledger/snapshot-160991176-5GMY4MtGzDnXYq9AVssRBxVyRj6YVRMjxfjZtqVBeP97.tar.zst http://tsolv.im-0.net:13899/snapshot-160991176-5GMY4MtGzDnXYq9AVssRBxVyRj6YVRMjxfjZtqVBeP97.tar.zst
+wget -O $HOME/ledger/snapshot-161660256-6makAMJ9P4eVnFLUVTrn8zY5N2YWxSXDRGutL7caR4uy.tar.zst http://tsolv.im-0.net:13899/snapshot-161660256-6makAMJ9P4eVnFLUVTrn8zY5N2YWxSXDRGutL7caR4uy.tar.zst
 
-wget -O $HOME/ledger/genesis.tar.bz2 http://api.testnet.solana.com/genesis.tar.bz2
+wget -O $HOME/ledger/genesis.tar.bz2  wget http://api.testnet.solana.com/genesis.tar.bz2
 
 sudo systemctl restart solana
