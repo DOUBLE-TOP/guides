7b779cfae5bf0bedfb44b32a86c21d0bfd1d50b4
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-12-12 18:42:02 +0200

    upd

--- tfsc/sync.sh
+++ tfsc/sync.sh
@@ -4,7 +4,7 @@ tmux kill-session -t tfsc
 
 rm -rf $HOME/tfsc/data.db
 
-wget -O $HOME/tfsc/data.zip  https://doubletop-bin.ams3.digitaloceanspaces.com/tfsc/data.db51070.zip
+wget -O $HOME/tfsc/data.zip  https://doubletop-bin.ams3.digitaloceanspaces.com/tfsc/data.db57704.zip
 
 cd $HOME/tfsc
 unzip data.zip
