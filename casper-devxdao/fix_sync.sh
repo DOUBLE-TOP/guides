fda13cba84a99afbedcf7405f86c972bb0fb984a
razumv <razumv95@gmail.com>
2022-12-16 15:18:03 +0100

    Update fix_sync.sh

--- casper-devxdao/fix_sync.sh
+++ casper-devxdao/fix_sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo systemctl stop casper-node-launcher
-TRUSTED_HASH=fa5f19d9c61d17a04550e5b0efad2cdd78599948afd886a68ec11af176c688a9
-if [ "$TRUSTED_HASH" != "null" ]; then sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/*/config.toml; fi
+height=$(curl -s http://127.0.0.1:8888/status | jq -r .last_added_block_info.height)
+step=20
+new_height=$(($height+$step))
+sudo sed -i "/trusted_hash =/c\trusted_hash = '$(casper-client get-block --node-address http://94.130.10.55:7777 -b $new_height | jq -r .result.block.hash | tr -d '\n')'" /etc/casper/*/config.toml
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo systemctl start casper-node-launcher
