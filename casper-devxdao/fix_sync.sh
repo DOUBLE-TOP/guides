5638d687f75703afeae21bb3b1650d9de6bdb1e0
razumv <razumv95@gmail.com>
2022-12-20 18:47:52 +0100

    Update fix_sync.sh

--- casper-devxdao/fix_sync.sh
+++ casper-devxdao/fix_sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo systemctl stop casper-node-launcher
 height=$(curl -s http://127.0.0.1:8888/status | jq -r .last_added_block_info.height)
+sudo systemctl stop casper-node-launcher
 step=20
 new_height=$(($height+$step))
 sudo sed -i "/trusted_hash =/c\trusted_hash = '$(casper-client get-block --node-address http://94.130.10.55:7777 -b $new_height | jq -r .result.block.hash | tr -d '\n')'" /etc/casper/*/config.toml
