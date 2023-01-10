da9d8196a624bed37bd7225489d52ce56e5563c7
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-01-10 16:17:59 +0200

    upd rolls

--- massa/rollsroyce.sh
+++ massa/rollsroyce.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #Thank's for https://raw.githubusercontent.com/bobu4/massa/main/bal.sh
 
-rm -f $HOME/massa/massa-client/massa-client
-if [ ! -e $HOME/massa/massa-client/massa-client ]; then
-  wget https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/massa/massa-client -O $HOME/massa/massa-client/massa-client
-  chmod +x $HOME/massa/massa-client/massa-client
-fi
-#
+# rm -f $HOME/massa/massa-client/massa-client
+# if [ ! -e $HOME/massa/massa-client/massa-client ]; then
+#   wget https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/massa/massa-client -O $HOME/massa/massa-client/massa-client
+#   chmod +x $HOME/massa/massa-client/massa-client
+# fi
+# #
 cd $HOME/massa/massa-client
-massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
+massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-        balance=$(./massa-client wallet_info | grep "Active rolls" | awk '{ print $3 }')
+        balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Rolls" | awk '{ print $3 }' | sed 's/final=//;s/,//')
         int_balance=${balance%%.*}
         if [ $int_balance -lt "1" ]; then
-                echo "Less than 1"
-                resp=$(./massa-client buy_rolls $massa_wallet_address 1 0)
+                echo "Less than 1 Final Roll"
+                resp=$(./massa-client --pwd $massa_pass buy_rolls $massa_wallet_address 1 0)
                 echo $resp
         elif [ $int_balance -gt "1" ]; then
-                echo "More than 1"
+                echo "More than 1 Final Roll"
         fi
         printf "sleep"
-        for((sec=0; sec<60; sec++))
+        for((sec=0; sec<3600; sec++))
         do
                 printf "."
                 sleep 1
