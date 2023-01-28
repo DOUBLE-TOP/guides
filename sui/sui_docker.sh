03bac7eb6a9ca50f20b7fe28fc0330e48d5355d4
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-01-28 12:25:31 +0200

    upd command check logs

--- sui/sui_docker.sh
+++ sui/sui_docker.sh
@@ -55,4 +55,4 @@ line
 run_docker
 line
 echo "installation complete, check logs by command:"
-echo "docker logs -f --tail=100 sui-fullnode-1"
+echo "docker-compose -f $HOME/sui/docker-compose.yaml logs -f --tail=100"
