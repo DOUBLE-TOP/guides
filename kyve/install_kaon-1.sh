6c7d34806f4b025cbbd551e90d2544786366f592
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-02-22 21:34:45 +0200

    fix

--- kyve/install_kaon-1.sh
+++ kyve/install_kaon-1.sh
@@ -37,7 +37,7 @@ kyved init $KYVE_NODENAME --chain-id $KYVE_CHAIN_ID &>/dev/null
 wget -O $HOME/.kyve/config/genesis.json https://snapshot.yeksin.net/kyve/genesis.json
 wget -qO $HOME/.kyve/config/addrbook.json https://snapshot.yeksin.net/kyve/addrbook.json &>/dev/null
 sed -i -e "s%^moniker *=.*%moniker = \"$KYVE_NODENAME\"%; "\
-"s%^external_address *=.*%external_address = \"`wget -qO- eth0.me`:26656\"%; " $HOME/.archway/config/config.toml
+"s%^external_address *=.*%external_address = \"`wget -qO- eth0.me`:26656\"%; " $HOME/.kyve/config/config.toml
 SEEDS=""
 PEERS="bc8b5fbb40a1b82dfba591035cb137278a21c57d@52.59.65.9:26656,801fa026c6d9227874eeaeba288eae3b800aad7f@52.29.15.250:26656,78d76da232b5a9a5648baa20b7bd95d7c7b9d249@142.93.161.118:26656,b68e5131552e40b9ee70427879eb34e146ef20df@18.194.131.3:26656,59addee10822d8cfe2c4635a404ab67687357449@141.95.33.158:26651,bbb7a427e04d38c74f574f6f0162e1359b66b330@93.115.25.18:39656,7258cf2c1867cc5b997baa19ff4a3e13681f14f4@68.183.143.17:26656,430845649afaad0a817bdf36da63b6f93bbd8bd1@3.67.29.225:26656,e8c9a0f07bc34fb870daaaef0b3da54dbf9c5a3b@15.235.10.35:26656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.kyve/config/config.toml
