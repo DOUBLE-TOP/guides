db8f615024f2866a80bacd95508fe1089f19864a
razumv <razumv95@gmail.com>
2023-02-26 10:18:20 +0100

    Update reanimate_main.sh

--- solana/reanimate_main.sh
+++ solana/reanimate_main.sh
@@ -12,57 +12,46 @@ Description=Solana MB Node
 After=network.target syslog.target
 StartLimitIntervalSec=0
 [Service]
-User=$USER
+User=root
 Type=simple
 Restart=always
 RestartSec=1
-LimitNOFILE=2024000
-Environment="SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password"
+LimitNOFILE=2048000
+Environment=SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password
 ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
---expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
---no-snapshot-fetch \
---wait-for-supermajority 179526403 \
---expected-shred-version 56177 \
---expected-bank-hash 69p75jzzT1P2vJwVn3wbTVutxHDcWKAgcbjqXvwCVUDE \
---known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
---known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
---known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
---known-validator C1ocKDYMCm2ooWptMMnpd5VEB2Nx4UMJgRuYofysyzcA \
---known-validator GwHH8ciFhR8vejWCqmg8FWZUCNtubPY2esALvy5tBvji \
---known-validator 6WgdYhhGE53WrZ7ywJA15hBVkw7CRbQ8yDBBTwmBtAHN \
---known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
 --entrypoint 3.15.228.179:11000 \
 --entrypoint 34.148.228.133:11000 \
 --entrypoint 63.251.232.254:21610 \
---entrypoint entrypoint.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
---entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
---only-known-rpc \
+--known-validator GjbUdcvguSNUnEeiTWa8f6wC1r1szUtUwKiJCp4oxjFX \
+--known-validator Cpcjz8kcLuxZLeTAxyKtCy1dtBb5kvYx35L1NBdmYKa7 \
+--known-validator AhWZi767Wo6sDWhXwS8oftvVWhvZj6FEa3175CBJ7Mbj \
+--known-validator 4z6YgEYt5U594UYfWh5dgYWEzpZLBqqfW9uQC7imzjne \
+--known-validator EBk1bP8LUmoWBHbaBQcwo7AiPtz6dw4yfFb5di8idzny \
+--known-validator Ch92wHj5wYDAXYT5BRgo3zwgeg5u6eLfiqZqbpw6Rpn5 \
+--no-snapshot-fetch \
+--expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --wal-recovery-mode skip_any_corrupted_record \
 --identity $HOME/mainnet-validator-keypair.json \
 --vote-account $HOME/vote-account-keypair.json \
 --ledger $HOME/ledger \
 --limit-ledger-size 50000000 \
---dynamic-port-range 8000-8020 \
+--dynamic-port-range 9030-9050 \
 --log $HOME/solana.log \
 --full-snapshot-interval-slots 25000 \
 --incremental-snapshot-interval-slots 500 \
---maximum-full-snapshots-to-retain 4 \
---maximum-incremental-snapshots-to-retain 20 \
---maximum-local-snapshot-age 3000 \
+--rpc-port 8899 \
 --full-rpc-api \
---private-rpc \
---rpc-port 8899 
-ExecReload=/bin/kill -s HUP $MAINPID
-ExecStop=/bin/kill -s QUIT $MAINPID
+--private-rpc
+ExecReload=/bin/kill -s HUP
+ExecStop=/bin/kill -s QUIT
 [Install]
 WantedBy=multi-user.target
 EOF"
 
 sudo systemctl daemon-reload
 
-wget -O $HOME/ledger/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst wget https://d1fe19ei6b1nmi.cloudfront.net/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst
+# wget -O $HOME/ledger/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst wget https://d1fe19ei6b1nmi.cloudfront.net/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst
 
 sudo systemctl restart solana
