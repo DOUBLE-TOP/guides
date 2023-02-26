c104fb3fd9d699524b1d3d1d1a1b0625d46bea08
razumv <razumv95@gmail.com>
2023-02-26 16:46:51 +0100

    Update reanimate_main.sh

--- solana/reanimate_main.sh
+++ solana/reanimate_main.sh
@@ -6,7 +6,7 @@ sudo systemctl stop solana
 
 sudo rm -rf $HOME/ledger/*
 
-bash -c "cat > $HOME/solana.service<<EOF
+bash -c "cat > /etc/systemd/system/solana.service<<EOF
 [Unit]
 Description=Solana MB Node
 After=network.target syslog.target
@@ -18,10 +18,12 @@ Restart=always
 RestartSec=1
 LimitNOFILE=2048000
 Environment=SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password
-ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
---entrypoint 3.15.228.179:11000 \
---entrypoint 34.148.228.133:11000 \
---entrypoint 63.251.232.254:21610 \
+ExecStart=/root/.local/share/solana/install/active_release/bin/solana-validator \
+--entrypoint 195.14.6.36:8001 \
+--entrypoint 198.244.229.6:8001 \
+--entrypoint 147.28.246.5:8001 \
+--entrypoint 147.28.151.175:8001 \
+--entrypoint 147.75.32.159:8001 \
 --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
 --known-validator GjbUdcvguSNUnEeiTWa8f6wC1r1szUtUwKiJCp4oxjFX \
@@ -33,21 +35,22 @@ ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator 
 --no-snapshot-fetch \
 --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --wal-recovery-mode skip_any_corrupted_record \
---identity $HOME/mainnet-validator-keypair.json \
---vote-account $HOME/vote-account-keypair.json \
---ledger $HOME/ledger \
+--identity /root/mainnet-validator-keypair.json \
+--vote-account /root/vote-account-keypair.json \
+--ledger /root/ledger \
 --limit-ledger-size 50000000 \
 --dynamic-port-range 9030-9050 \
---log $HOME/solana.log \
+--log /root/solana.log \
 --full-snapshot-interval-slots 25000 \
 --incremental-snapshot-interval-slots 500 \
 --rpc-port 8899 \
 --full-rpc-api \
 --private-rpc
-ExecReload=/bin/kill -s HUP
-ExecStop=/bin/kill -s QUIT
+ExecReload=/bin/kill -s HUP 
+ExecStop=/bin/kill -s QUIT 
 [Install]
 WantedBy=multi-user.target
+
 EOF"
 
 sudo systemctl daemon-reload
