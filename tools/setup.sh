8c47585983726a3bbe5f4d7259469c09afa83298
razumv <razumv95@gmail.com>
2023-03-13 15:08:01 +0100

    Update setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -12,45 +12,43 @@ proxy="https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/3proxy.sh"
 
 confirm=$(dialog --stdout --yesno "Do you want to install $node with option $option?" 0 0)
 # Show the main menu
-while true; do
-    if [ "$2" = "main" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $main)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    elif [ "$2" = "monitoring" ]; then
-        if [ "$confirm" = "0" ]; then
-            OWNER=$(dialog --inputbox "Enter telegram username without @:" 0 0 "John" --stdout)
-            HOSTNAME=$(dialog --inputbox "Enter server name without special symbols:" 0 0 "server" --stdout)
-            . <(wget -qO- $monitoring)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    if [ "$2" = "docker" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $docker)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    elif [ "$2" = "rust" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $rust)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    if [ "$2" = "go" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $go)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    elif [ "$2" = "nodejs" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $nodejs)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    if [ "$2" = "proxy" ]; then
-        if [ "$confirm" = "0" ]; then
-            . <(wget -qO- $proxy)
-            dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
-        fi
-    else
-    dialog --title "Installation cancelled" --msgbox "The installation was cancelled." 0 0
+if [ "$2" = "main" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $main)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
     fi
-done
+elif [ "$2" = "monitoring" ]; then
+    if [ "$confirm" = "0" ]; then
+        OWNER=$(dialog --inputbox "Enter telegram username without @:" 0 0 "John" --stdout)
+        HOSTNAME=$(dialog --inputbox "Enter server name without special symbols:" 0 0 "server" --stdout)
+        . <(wget -qO- $monitoring)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+elif [ "$2" = "docker" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $docker)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+elif [ "$2" = "rust" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $rust)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+elif [ "$2" = "go" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $go)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+elif [ "$2" = "nodejs" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $nodejs)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+elif [ "$2" = "proxy" ]; then
+    if [ "$confirm" = "0" ]; then
+        . <(wget -qO- $proxy)
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+    fi
+else
+    dialog --title "Installation cancelled" --msgbox "The installation was cancelled." 0 0
+fi
