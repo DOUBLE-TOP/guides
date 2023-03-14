0627a7767f441d835393ec1fd26797254133ba11
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-03-14 11:56:15 +0200

    upd

--- penumbra/autostake.sh
+++ penumbra/autostake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
-echo "-----------------------------------------------------------------------------"
+#echo "-----------------------------------------------------------------------------"
+#curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+#echo "-----------------------------------------------------------------------------"
 
 validator=penumbravalid1xtdga92ppkhdadyc7s4qws3y547ukalz5cyqp957z2cn594w9cqsl34tjv
 echo "-----------------------------------------------------------------------------"
@@ -9,7 +9,7 @@ echo "Прописываем валидатора"
 echo $validator
 echo "-----------------------------------------------------------------------------"
 
-current_balance=$(pcli view balance | grep penumbra | awk '{ print $2 }' | tr -d 'penumbra' | head -1)
+current_balance=$(pcli view balance | grep penumbra | awk '{ print $2 }' | tr -d 'penumbra' | head -1) &>/dev/null
 dif=10
 sleep 15
 stake_balance=$(($current_balance-$dif))
@@ -17,7 +17,7 @@ echo "--------------------------------------------------------------------------
 echo "Выводим баланс для стейкинга "
 echo $stake_balance
 echo "-----------------------------------------------------------------------------"
-stake=$(pcli tx delegate "${stake_balance}"penumbra --to $validator)
+stake=$(pcli tx delegate "${stake_balance}"penumbra --to $validator) &>/dev/null
 sleep 15
 echo "-----------------------------------------------------------------------------"
 echo "Стейкаем монетки валидатору"
---
+++ penumbra/update_stake.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+function logo {
+  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+}
+
+function line {
+  echo "-----------------------------------------------------------------------------"
+}
+
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
+
+function install_tools {
+  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
+  source ~/.cargo/env
+  rustup default nightly
+  sleep 1
+}
+
+function source_git {
+  if [ ! -d $HOME/penumbra/ ]; then
+    git clone https://github.com/penumbra-zone/penumbra
+  fi
+  cd $HOME/penumbra
+  git reset --hard
+  git fetch
+  git checkout $version && cargo update
+}
+
+# function build_penumbra {
+#   if [ ! -d $HOME/penumbra/ ]; then
+#     cd $HOME/penumbra/
+#     cargo build --release --bin pcli
+#     sudo rm -f /usr/bin/pcli
+#     sudo cp target/release/pcli /usr/bin/pcli
+#   else
+#     source_git
+#     cd $HOME/penumbra/
+#     cargo build --release --bin pcli
+#     sudo rm -f /usr/bin/pcli
+#     sudo cp target/release/pcli /usr/bin/pcli
+#   fi
+# }
+
+function wget_bin_pcli {
+  mkdir -p $HOME/penumbra/target/release/
+  wget -O  $HOME/penumbra/target/release/pcli https://doubletop-bin.ams3.digitaloceanspaces.com/penumbra/$version/pcli
+  sudo chmod +x $HOME/penumbra/target/release/pcli
+  sudo cp $HOME/penumbra/target/release/pcli /usr/bin/pcli
+}
+
+function generate_wallet {
+  cd $HOME/penumbra/
+  pcli keys generate
+}
+
+function reset_wallet {
+  cd $HOME/penumbra/
+  pcli view reset
+}
+
+function stake {
+  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/penumbra/autostake.sh | bash
+}
+
+# function rust_update {
+#   source ~/.cargo/env
+#   rustup update
+#   rustup default nightly
+# }
+
+
+colors
+export version="048-carme"
+source_git
+# rust_update
+
+# build_penumbra
+wget_bin_pcli
+reset_wallet
+stake
