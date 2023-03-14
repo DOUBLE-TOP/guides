27bdf6dac3b6cfb638b2c7187cc78c107e1e8538
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-03-14 11:38:21 +0200

    first commit

---
+++ penumbra/autostake.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+validator=penumbravalid1xtdga92ppkhdadyc7s4qws3y547ukalz5cyqp957z2cn594w9cqsl34tjv
+echo "-----------------------------------------------------------------------------"
+echo "Прописываем валидатора"
+echo $validator
+echo "-----------------------------------------------------------------------------"
+
+current_balance=$(pcli view balance | grep penumbra | awk '{ print $1 }' | tr -d 'penumbra' | head -1)
+dif=10
+sleep 15
+stake_balance=$(($current_balance-$dif))
+echo "-----------------------------------------------------------------------------"
+echo "Выводим баланс для стейкинга "
+echo $stake_balance
+echo "-----------------------------------------------------------------------------"
+stake=$(pcli tx delegate "${stake_balance}"penumbra --to $validator)
+sleep 15
+echo "-----------------------------------------------------------------------------"
+echo "Стейкаем монетки валидатору"
+echo $stake
+echo "-----------------------------------------------------------------------------"
