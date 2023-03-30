52e46cbc6283a319b3c84676e2ce8ebe2f7e25ba
razumv <razumv95@gmail.com>
2023-03-30 22:06:07 +0200

    fix :+1:

--- shardeum/setup.sh
+++ shardeum/setup.sh
@@ -3,7 +3,7 @@
 node=$1
 option=$2
 
-install="https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh"
+install="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/install.sh"
 healthcheck="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/health.sh"
 
 confirm=$(dialog --clear --stdout --yesno "Do you want to install $node with option $option?" 0 0)
@@ -17,6 +17,13 @@ fi
 # Show the main menu
 if [ "$option" = "install" ]; then
     if [ "$confirm" != "0" ]; then
+        WARNING_AGREE=y
+        RUNDASHBOARD=y
+        DASHPASS=$(dialog --inputbox "Set the password to access the Dashboard(without spec symbols):" 0 0 "qwerty123" --stdout)
+        DASHPORT=$(dialog --inputbox "Enter the port (1025-65536) to access the web based Dashboard:" 0 0 "38080" --stdout)
+        SHMEXT=$(dialog --inputbox "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication:" 0 0 "19001" --stdout)
+        SHMINT=$(dialog --inputbox "Enter the second port (1025-65536) for p2p communication:" 0 0 "20001" --stdout)
+        NODEHOME=$(dialog --inputbox "What base directory should the node use:" 0 0 "$HOME/.shardeum" --stdout)
         . <(wget -qO- $install)
         cd $HOME
         dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful! Stake your tokens in node: https://$(curl -s https://api.ipify.org):8080/maintenance" 0 0
