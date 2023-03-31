7da4084434db67705bed43a6cf8e7ddced39fdb4
razumv <razumv95@gmail.com>
2023-03-31 15:26:35 +0200

    Update install.sh

--- shardeum/install.sh
+++ shardeum/install.sh
@@ -1,51 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
-
-get_ip() {
-  local ip
-  if command -v ip >/dev/null; then
-    ip=$(ip addr show $(ip route | awk '/default/ {print $5}') | awk '/inet/ {print $2}' | cut -d/ -f1 | head -n1)
-  elif command -v netstat >/dev/null; then
-    interface=$(netstat -rn | awk '/default/{print $4}' | head -n1)
-    ip=$(ifconfig "$interface" | awk '/inet /{print $2}')
-  else
-    echo "Error: neither 'ip' nor 'ifconfig' command found. Submit a bug for your OS."
-    return 1
-  fi
-  echo $ip
-}
-
-get_external_ip() {
-  external_ip=''
-  external_ip=$(curl -s https://api.ipify.org)
-  if [[ -z "$external_ip" ]]; then
-    external_ip=$(curl -s http://checkip.dyndns.org | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
-  fi
-  if [[ -z "$external_ip" ]]; then
-    external_ip=$(curl -s http://ipecho.net/plain)
-  fi
-  if [[ -z "$external_ip" ]]; then
-    external_ip=$(curl -s https://icanhazip.com/)
-  fi
-    if [[ -z "$external_ip" ]]; then
-    external_ip=$(curl --header  "Host: icanhazip.com" -s 104.18.114.97)
-  fi
-  if [[ -z "$external_ip" ]]; then
-    external_ip=$(get_ip)
-    if [ $? -eq 0 ]; then
-      echo "The IP address is: $IP"
-    else
-      external_ip="localhost"
-    fi
-  fi
-  echo $external_ip
-}
-
-APPSEEDLIST="archiver-sphinx.shardeum.org"
-APPMONITOR="monitor-sphinx.shardeum.org"
 
 if [ -d "$NODEHOME" ]; then
   if [ "$NODEHOME" != "$(pwd)" ]; then
@@ -56,13 +9,14 @@ if [ -d "$NODEHOME" ]; then
   fi
 fi
 
-git clone https://gitlab.com/shardeum/validator/dashboard.git ${NODEHOME} &&
-  cd ${NODEHOME} &&
-  chmod a+x ./*.sh
+git clone https://gitlab.com/shardeum/validator/dashboard.git ${NODEHOME} && cd ${NODEHOME} && chmod a+x ./*.sh
 
-SERVERIP=$(get_external_ip)
-LOCALLANIP=$(get_ip)
-cd ${NODEHOME} &&
+APPSEEDLIST="archiver-sphinx.shardeum.org"
+APPMONITOR="monitor-sphinx.shardeum.org"
+SERVERIP=$(curl -s https://api.ipify.org)
+LOCALLANIP=$(curl -s https://api.ipify.org)
+
+cd ${NODEHOME}
 touch ./.env
 cat >./.env <<EOL
 APP_IP=auto
@@ -77,24 +31,16 @@ SHMINT=${SHMINT}
 EOL
 
 docker-compose down -v
-docker rmi -f test-dashboard
-docker rmi -f local-dashboard
-docker rmi -f registry.gitlab.com/shardeum/server
+docker rmi -f test-dashboard registry.gitlab.com/shardeum/server local-dashboard
 
-cd ${NODEHOME} &&
+
 docker build --no-cache -t local-dashboard -f Dockerfile --build-arg RUNDASHBOARD=${RUNDASHBOARD} .
 
-cd ${NODEHOME}
-if [[ "$(uname)" == "Darwin" ]]; then
-  sed "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.tmpl > docker-compose.yml
-  sed -i '' "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
-  sed -i '' "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
-else
-  sed "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.tmpl > docker-compose.yml
-  sed -i "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
-  sed -i "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
-fi
+
+sed "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.tmpl > docker-compose.yml
+sed -i "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
+sed -i "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
 docker-compose up -d
 
-# echo "Starting image. This could take a while..."
-# (docker logs -f shardeum-dashboard &) | grep -q 'done'
+echo "Starting image. This could take a while..."
+(docker logs -f shardeum-dashboard &) | grep -q 'done'
