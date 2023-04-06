19d59f99235fe99a632210dd5b78dd8779c9df58
razumv <razumv95@gmail.com>
2023-04-06 16:49:13 +0200

    migrate tools to --force :+1:

--- tools/setup.sh
+++ tools/setup.sh
@@ -12,50 +12,66 @@ nodejs="https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/node.sh"
 proxy="https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/3proxy.sh"
 
 confirm=$(dialog --clear --stdout --yesno "Do you want to install $node with option $option?" 0 0)
-
+text="The installation was successful!"
 if [ "$?" -eq 0 ]; then
     confirm=1
 else
     confirm=0
 fi
+if [[ $# -eq 3 ]]; then
+  if [ "$3" == "--force" ]; then
+    force="true"
+  fi
+fi
+
+dialog_text() {
+    if [ -z "$force" ] || [ "$force" != "true" ]; then
+        dialog --title "Installation complete" --msgbox "$text" 0 0
+    fi
+}
 
 # Show the main menu
 if [ "$option" = "main" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $main) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 elif [ "$option" = "monitoring" ]; then
     if [ "$confirm" != "0" ]; then
-        OWNER=$(dialog --inputbox "Enter telegram username without @:" 0 0 "John" --stdout)
+        if [ ! $OWNER ]; then
+            OWNER=$(dialog --inputbox "Enter telegram username without @:" 0 0 "John" --stdout)
+        fi
+        if [ ! $HOSTNAME ]; then
         HOSTNAME=$(dialog --inputbox "Enter server name without special symbols:" 0 0 "server" --stdout)
+        fi
         . <(wget -qO- $monitoring) &>/dev/null
-        dialog --title "Installation complete" --msgbox "Monitoring installed! Your link in Grafana is - https://grafana.razumv.tech/d/xfpJB9FGz123/nodes-doubletop?orgId=1&var-origin_prometheus=&var-job=node_exporter&var-owner=$OWNER&var-hostname=All&var-node=$HOSTNAME&var-device=All&var-interval=2m&var-maxmount=%2F&var-show_hostname=subq&var-total=3" 0 0
+        text="Monitoring installed! Your link in Grafana is - https://grafana.razumv.tech/d/xfpJB9FGz123/nodes-doubletop?orgId=1&var-origin_prometheus=&var-job=node_exporter&var-owner=$OWNER&var-hostname=All&var-node=$HOSTNAME&var-device=All&var-interval=2m&var-maxmount=%2F&var-show_hostname=subq&var-total=3"
+        dialog_text
     fi
 elif [ "$option" = "docker" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $docker) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 elif [ "$option" = "rust" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $rust) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 elif [ "$option" = "go" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $go) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 elif [ "$option" = "nodejs" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $nodejs) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 elif [ "$option" = "proxy" ]; then
     if [ "$confirm" != "0" ]; then
         . <(wget -qO- $proxy) &>/dev/null
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful!" 0 0
+        dialog_text
     fi
 else
     dialog --title "Installation cancelled" --msgbox "The installation was cancelled." 0 0
