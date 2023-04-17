a9955113fa6df5b212114d1a62d177b1dce2f1de
razumv <razumv95@gmail.com>
2023-04-17 12:27:47 +0200

    Update update_gear.sh

--- Gear/update_gear.sh
+++ Gear/update_gear.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-source $HOME/.profile
-sudo systemctl stop gear
-/root/gear purge-chain -y
+# source $HOME/.profile
+# sudo systemctl stop gear
+# /root/gear purge-chain -y
 
-wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz
-sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
-rm gear-nightly-linux-x86_64.tar.xz
+# wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz
+# sudo tar -xvf gear-nightly-linux-x86_64.tar.xz -C /root
+# rm gear-nightly-linux-x86_64.tar.xz
 
 
-sudo systemctl start gear
+# sudo systemctl start gear
 
-sleep 15
+# sleep 15
 
 NODENAME_GEAR=$(grep -Po '(?<=--name\s)\S+(?=\s*--execution)' /etc/systemd/system/gear.service)
 
@@ -49,8 +49,6 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl daemon-reload
-sudo systemctl stop gear
 # cd /root/.local/share/gear/chains
 # mkdir -p gear_staging_testnet_v7/network/
 # sudo cp gear_staging_testnet_v6/network/secret_ed25519 gear_staging_testnet_v7/network/secret_ed25519  &>/dev/null
