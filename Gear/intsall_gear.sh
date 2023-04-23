27b67fc0e8c7b9b9df7e15e4ec50b7b50a0901e5
razumv <razumv95@gmail.com>
2023-04-23 15:13:09 +0200

    fix reserved-nodes

--- Gear/intsall_gear.sh
+++ Gear/intsall_gear.sh
@@ -43,11 +43,6 @@ ExecStart=$HOME/gear \
 	--port 31333 \
         --telemetry-url 'ws://telemetry-backend-shard.gear-tech.io:32001/submit 0' \
 	--telemetry-url 'wss://telemetry.doubletop.io/submit 0' \
-        --reserved-nodes \
-        "/dns4/testnet-validator-node1.gear-tech.io/tcp/30333/p2p/12D3KooWFqktBAWLLvdySqG5QMcxHnpsDi8vjR9rjCxHotXyXn5R" \
-        "/dns4/testnet-validator-node2.gear-tech.io/tcp/30333/p2p/12D3KooWN2Rv9aLGqJ1RohQ9HoYe1nf88Np1M56SZvyWi8rGon36" \
-        "/dns4/testnet-validator-node3.gear-tech.io/tcp/30333/p2p/12D3KooWEVvqVD2mrLfmgeX1EXZ2caFXXEWWEs4Taa4mWzFUoF34" \
-        "/dns4/testnet-validator-node4.gear-tech.io/tcp/30333/p2p/12D3KooWSf2d69w7RYKtj9mgYpLDs3rqLAz9GHNSHHoCQDLUjeiP" \
         --in-peers 200 \
         --out-peers 200
 
