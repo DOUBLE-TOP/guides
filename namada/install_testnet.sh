90888685fbbe99b08eb5eab5724630ea3e822ec0
razumv <razumv95@gmail.com>
2023-04-28 11:42:58 +0200

    public-testnet-7.0.3c5a38dc983

--- namada/install_testnet.sh
+++ namada/install_testnet.sh
@@ -44,7 +44,7 @@ function NAMADA_NAME {
 function vars {
   echo "export NAMADA_TAG=v0.15.1" >> ~/.bash_profile
   echo "export TM_HASH=v0.1.4-abciplus" >> ~/.bash_profile
-  echo "export CHAIN_ID=public-testnet-6.0.a0266444b06" >> ~/.bash_profile
+  echo "export CHAIN_ID=public-testnet-7.0.3c5a38dc983" >> ~/.bash_profile
   echo "export VALIDATOR_ALIAS=$NAMADA_NAME" >> ~/.bash_profile
   echo "export WALLET=$NAMADA_NAME" >> ~/.bash_profile
   source ~/.bash_profile
