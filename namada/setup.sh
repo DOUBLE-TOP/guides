a564c674d3d31b9d28ebc15f934d4507b7f6a2b2
razumv <razumv95@gmail.com>
2023-04-28 11:22:13 +0200

    update namada

---
+++ namada/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+node=$1
+option=$2
+
+install="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/namada/install.sh"
+update="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/namada/update.sh"
+
+confirm=$(dialog --clear --stdout --yesno "Do you want to apply $option to  $node?" 0 0)
+
+if [ "$?" -eq 0 ]; then
+    confirm=1
+else
+    confirm=0
+fi
+
+# Show the main menu
+if [ "$option" = "install" ]; then
+    if [ "$confirm" != "0" ]; then
+        NAMADA_NAME=$(dialog --inputbox "Set nodename for Namada(without spec symbols):" 0 0 "qwerty123" --stdout)
+        . <(wget -qO- $install) &>/dev/null
+        cd $HOME
+        dialog --title "Installation complete" --msgbox "The installation of $node was successful!" 0 0
+    fi
+elif [ "$option" = "update" ]; then
+    if [ "$confirm" != "0" ]; then
+        . <(wget -qO- https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
+        . <(wget -qO- $update) &>/dev/null
+        cd $HOME
+        dialog --title "Healthcheck enabled" --msgbox "Update for $node was successful!" 0 0
+    fi
+elif [ "$option" = "delete" ]; then
+    if [ "$confirm" != "0" ]; then
+        sudo systemctl stop namada && sudo systemctl disable namada
+        rm -rf /etc/systemd/system/namadad* $HOME/tendermint $HOME/.namada* $HOME/namada
+        dialog --title "delete" --msgbox "$node was successful deleted!" 0 0
+    fi
+else
+    dialog --title "Installation cancelled" --msgbox "The installation was cancelled." 0 0
+fi
