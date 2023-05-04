900072fc506e2334a692674ac301ac2fef072b69
razumv <razumv95@gmail.com>
2023-05-04 18:11:12 +0200

    fix

--- shardeum/stake.sh
+++ shardeum/stake.sh
@@ -4,5 +4,6 @@ PRIV_KEY=$(dialog --inputbox "Enter your private key from your testnet Shardeum 
 WALLET_ADDR=$(dialog --inputbox "Enter your wallet address from your testnet Shardeum node(Metamask wallet) :" 0 0 "0x000000......" --stdout) && clear
 
 docker exec -it shardeum-dashboard operator-cli start
+sleep 30
 docker exec -it -e PRIV_KEY=$PRIV_KEY shardeum-dashboard operator-cli stake 10
 docker exec -it -e WALLET_ADDR=$WALLET_ADDR shardeum-dashboard operator-cli stake_info $WALLET_ADDR
