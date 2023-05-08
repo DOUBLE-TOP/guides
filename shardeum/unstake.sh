26bd491bc32e4a0b50fa62327b1b01f273e5469f
razumv <razumv95@gmail.com>
2023-05-08 18:34:05 +0200

    апдейт

--- shardeum/unstake.sh
+++ shardeum/unstake.sh
@@ -3,4 +3,5 @@ PRIV_KEY=$(dialog --inputbox "Enter your private key from your testnet Shardeum 
 WALLET_ADDR=$(dialog --inputbox "Enter your wallet address from your testnet Shardeum node(Metamask wallet) :" 0 0 "0x000000......" --stdout) && clear
 
 docker exec -it -e WALLET_ADDR=$WALLET_ADDR shardeum-dashboard operator-cli stake_info $WALLET_ADDR
-docker exec -it -e PRIV_KEY=$PRIV_KEY shardeum-dashboard operator-cli unstake -f
+# docker exec -it -e PRIV_KEY=$PRIV_KEY shardeum-dashboard operator-cli unstake -f
+docker exec -it shardeum-dashboard sh -c "(sleep 5; echo '${PRIV_KEY}'; sleep 5) | operator-cli unstake -f"
