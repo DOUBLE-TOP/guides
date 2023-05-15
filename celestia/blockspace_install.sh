fd11e39967653610aa206493b83f76cfbea041e2
razumv <42905535+razumv@users.noreply.github.com>
2023-05-15 21:32:34 +0200

    Update blockspace_install.sh

--- celestia/blockspace_install.sh
+++ celestia/blockspace_install.sh
@@ -23,7 +23,7 @@ function source_build_code {
 
     git clone https://github.com/celestiaorg/celestia-node.git
     cd $HOME/celestia-node
-    git checkout tags/v0.9.4
+    git checkout tags/v0.9.5
     make build
     make install
     make cel-key
