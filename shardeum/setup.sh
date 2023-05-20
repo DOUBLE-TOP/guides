a726cb8dfd33bc36d208e4f82da5b00bc4ac8f33
razumv <razumv95@gmail.com>
2023-05-20 16:32:43 +0200

    update shardeum

--- shardeum/setup.sh
+++ shardeum/setup.sh
@@ -3,7 +3,9 @@
 node=$1
 option=$2
 
-install="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/install.sh"
+main="https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh"
+docker="https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh"
+install="https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh"
 healthcheck="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/health.sh"
 unstake="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/unstake.sh"
 stake="https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/shardeum/stake.sh"
@@ -19,44 +21,15 @@ fi
 # Show the main menu
 if [ "$option" = "install" ]; then
     if [ "$confirm" != "0" ]; then
-        WARNING_AGREE=y
-        RUNDASHBOARD=y
-        DASHPASS=$(dialog --inputbox "Set the password to access the Dashboard(without spec symbols):" 0 0 "qwerty123" --stdout)
-        while true; do
-            DASHPORT=$(dialog --inputbox "Enter the port (1025-65536) to access the web based Dashboard:" 0 0 "8080" --stdout)
-            # Проверка, что порт свободен
-            if ! nc -z localhost "$DASHPORT"; then
-                break
-            else
-                dialog --msgbox "Port $DASHPORT is already in use. Please choose another port." 0 0
-            fi
-        done
-        while true; do
-            SHMEXT=$(dialog --inputbox "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication:" 0 0 "29001" --stdout)
-            # Проверка, что порт свободен
-            if ! nc -z localhost "$SHMEXT"; then
-                break
-            else
-                dialog --msgbox "Port $SHMEXT is already in use. Please choose another port." 0 0
-            fi
-        done
-        while true; do
-            SHMINT=$(dialog --inputbox "Enter the second port (1025-65536) for p2p communication:" 0 0 "30001" --stdout)
-            # Проверка, что порт свободен
-            if ! nc -z localhost "$SHMINT"; then
-                break
-            else
-                dialog --msgbox "Port $SHMINT is already in use. Please choose another port." 0 0
-            fi
-        done
-        NODEHOME=$(dialog --inputbox "What base directory should the node use:" 0 0 "$HOME/.shardeum" --stdout)
-        . <(wget -qO- $install) &>/dev/null
+        . <(wget -qO- $main) &>/dev/null
+        . <(wget -qO- $docker) &>/dev/null
+        . <(wget -qO- $install)
+        source $HOME/.shardeum/.env
         cd $HOME
-        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful! Stake your tokens in node: https://$(curl -s https://api.ipify.org):$DASHPORT/maintenance" 0 0
+        dialog --title "Installation complete" --msgbox "The installation of $node with option $option was successful! Stake your tokens in node: https://$SERVERIP:$DASHPORT/maintenance" 0 0
     fi
 elif [ "$option" = "healthcheck" ]; then
     if [ "$confirm" != "0" ]; then
-        . <(wget -qO- https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
         tmux new-session -d -s shardeum_healthcheck '. <(wget -qO- $healthcheck)'
         cd $HOME
         dialog --title "Healthcheck enabled" --msgbox "Healthcheck enabled for $node was successful!" 0 0
