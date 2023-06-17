b11e349acc8cb3f6f40bbb41b4989ac36e948b30
KURASH <92112412+kuraassh@users.noreply.github.com>
2023-06-17 15:14:57 +0300

    upd

--- massa/install.sh
+++ massa/install.sh
@@ -16,7 +16,7 @@ rustup default nightly
 cd $HOME
 if [ ! -d $HOME/massa/ ]; then
 	git clone https://github.com/massalabs/massa
-	cd $HOME/massa && git checkout TEST.22.2
+	cd $HOME/massa && git checkout TEST.23.2
 fi
 cd $HOME/massa/massa-node/
 cargo build --release
