a117c3871f33a5bba77ebe3944a2aac9859e6183
razumv <razumv95@gmail.com>
2023-06-17 14:22:44 +0200

    update to 23.1

--- massa/update.sh
+++ massa/update.sh
@@ -20,20 +20,21 @@ function get_env {
 }
 
 function massa_backup {
+	rm -rf $HOME/massa_backup/
 	cd $HOME
-	if [ ! -d $HOME/massa_backup22/ ]; then
-		mkdir -p $HOME/massa_backup22
-		cp $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_backup22/
-		cp $HOME/massa/massa-client/wallet.dat $HOME/massa_backup22/
+	if [ ! -d $HOME/massa_backup/ ]; then
+		mkdir -p $HOME/massa_backup
+		cp $HOME/massa/massa-node/config/node_privkey.key $HOME/massa_backup/
+		cp $HOME/massa/massa-client/wallet.dat $HOME/massa_backup/
 	fi
-	if [ ! -e $HOME/massa_backup22.tar.gz ]; then
-		tar cvzf massa_backup.tar22.gz massa_backup22
+	if [ ! -e $HOME/massa_backup.tar.gz ]; then
+		tar cvzf massa_backup.tar.gz massa_backup
 	fi
 }
 
 function delete {
   sudo systemctl stop massa
-  rm -rf massa_TEST.2*
+  rm -rf massa_TEST.*
   rm -rf $HOME/massa
 }
 
@@ -47,32 +48,9 @@ function routable_ip {
   sed -i "/\[network\]/a routable_ip=\"$(curl -s ifconfig.me)\"" "$HOME/massa/massa-node/base_config/config.toml"
 }
 
-function replace_bootstraps {
-  	config_path="$HOME/massa/massa-node/base_config/config.toml"
-  	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
-  	len=`wc -l < "$config_path"`
-  	start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
-  	end=`grep -n "\[optionnal\] port on which to listen" "$config_path" | cut -d: -f1`
-  	end=$((end-1))
-  	first_part=`sed "${start},${len}d" "$config_path"`
-  	second_part="
-      bootstrap_list = [
-  ${bootstrap_list}
-      ]
-  "
-  	third_part=`sed "1,${end}d" "$config_path"`
-  	echo "${first_part}${second_part}${third_part}" > "$config_path"
-  	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
-}
-
 function keys_from_backup {
-	cp $HOME/massa_backup20/wallet.dat $HOME/massa/massa-client/wallet.dat
-	cp $HOME/massa_backup20/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
-}
-
-function alias {
-  echo "alias client='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
-  echo "alias clientw='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
+	cp $HOME/massa_backup/wallet.dat $HOME/massa/massa-client/wallet.dat
+	cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 }
 
 colors
@@ -84,9 +62,6 @@ delete
 
 install
 routable_ip
-#replace_bootstraps
-# alias
-
-#keys_from_backup
+keys_from_backup
 
 sudo systemctl start massa
