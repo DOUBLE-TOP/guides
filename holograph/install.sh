bccd48a150d8756aec5e0e28368678001e6d16c8
razumv <42905535+razumv@users.noreply.github.com>
2023-06-23 14:17:11 +0200

    Update install.sh

--- holograph/install.sh
+++ holograph/install.sh
@@ -38,31 +38,6 @@ function holograph_operator {
     holograph operator:bond
 }
 
-function systemd_holograph {
-    sudo tee /etc/systemd/system/holographd.service > /dev/null <<EOF
-[Unit]
-Description=Holograph
-After=network.target
-
-[Service]
-Type=simple
-User=root
-ExecStart=holograph operator --mode=auto --unsafePassword=2topnodes --sync
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable holographd &>/dev/null
-sudo systemctl restart holographd
-}
-
-
-
 colors
 line
 logo
@@ -85,7 +60,4 @@ line
 echo "bonding into a pod"
 holograph_operator
 line
-echo "creating systemd file, adding to autostart, starting"
-systemd_holograph
-echo "installation complete, check logs by command:"
-echo "sudo journalctl -u holographd -f -o cat"
+
