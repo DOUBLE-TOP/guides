0281b2f266dcc6ea078123be5f6216a58e37ede2
razumv <42905535+razumv@users.noreply.github.com>
2023-07-04 23:37:09 +0200

    Update update.sh

--- massa/update.sh
+++ massa/update.sh
@@ -39,7 +39,7 @@ function delete {
 }
 
 function install {
-  wget https://github.com/massalabs/massa/releases/download/TEST.23.2/massa_TEST.24.0_release_linux.tar.gz
+  wget https://github.com/massalabs/massa/releases/download/TEST.24.0/massa_TEST.24.0_release_linux.tar.gz
   tar zxvf massa_TEST.24.0_release_linux.tar.gz -C $HOME/
 }
 
