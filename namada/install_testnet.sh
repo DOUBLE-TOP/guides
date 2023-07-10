800e33ec194554f85218ac81292b9ca0688b4618
razumv <razumv95@gmail.com>
2023-07-10 11:50:10 +0200

    фикс

--- namada/install_testnet.sh
+++ namada/install_testnet.sh
@@ -96,7 +96,7 @@ function join_network {
   cd $HOME
   namada client utils join-network --chain-id $CHAIN_ID
   mkdir -p $HOME/.local/share/namada/${CHAIN_ID}/tendermint/config/
-  get -O $HOME/.local/share/namada/${CHAIN_ID}/cometbft/config/addrbook.json https://raw.githubusercontent.com/McDaan/general/main/namada/addrbook.json
+  wget -O $HOME/.local/share/namada/${CHAIN_ID}/cometbft/config/addrbook.json https://raw.githubusercontent.com/McDaan/general/main/namada/addrbook.json
   sudo sed -i 's/0\.0\.0\.0:26656/0\.0\.0\.0:51656/g; s/127\.0\.0\.1:26657/127\.0\.0\.1:51657/g; s/127\.0\.0\.1:26658/127\.0\.0\.1:51658/g' $HOME/.local/share/namada/public-testnet*/config.toml
 }
 
