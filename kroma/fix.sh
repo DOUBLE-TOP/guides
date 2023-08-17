47d850407e81b93b57538ccf7c8c8da452774da2
razumv <razumv95@gmail.com>
2023-08-17 16:04:15 +0200

    Update fix.sh

--- kroma/fix.sh
+++ kroma/fix.sh
@@ -8,8 +8,8 @@ docker compose down -v
 cd $HOME
 rm -rf /home/geth/eth-docker/
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/sepolia.sh)
-sleep 180m
+sleep 360m
 cd /root/kroma-up && docker compose --profile validator up -d
 bash $HOME/kroma-up/sync_block.sh
-sleep 30m
+sleep 5m
 docker exec kroma-validator kroma-validator deposit --amount 600000000000000000
