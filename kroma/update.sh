0fab415aee27ff6ae2f4fcac959774c990d18a00
razumv <razumv95@gmail.com>
2023-08-31 11:50:03 +0200

    Update update.sh

--- kroma/update.sh
+++ kroma/update.sh
@@ -8,7 +8,7 @@ ufw disable
 docker-compose -f $HOME/kroma-up/docker-compose.yml --profile validator down
 
 KROMA_VALIDATOR__PRIVATE_KEY=$(cat $HOME/kroma-up/.env | grep KROMA_VALIDATOR__PRIVATE_KEY | awk -F'=' '{print $2}')
-L1_RPC_ENDPOINT=$(cat $HOME/kroma-up/.env | grep L1_RPC_ENDPOINT | awk -F'=' '{print $2}')
+# L1_RPC_ENDPOINT=$(cat $HOME/kroma-up/.env | grep L1_RPC_ENDPOINT | awk -F'=' '{print $2}')
 
 cd $HOME/kroma-up
 
@@ -20,7 +20,7 @@ git pull origin main
 cp .env.sample .env
 
 sed -i "s|KROMA_VALIDATOR__PRIVATE_KEY=.*|KROMA_VALIDATOR__PRIVATE_KEY=$KROMA_VALIDATOR__PRIVATE_KEY|" $HOME/kroma-up/.env
-sed -i "s|L1_RPC_ENDPOINT=.*|L1_RPC_ENDPOINT=$L1_RPC_ENDPOINT|" $HOME/kroma-up/.env
+# sed -i "s|L1_RPC_ENDPOINT=.*|L1_RPC_ENDPOINT=$L1_RPC_ENDPOINT|" $HOME/kroma-up/.env
 
 sed -i 's/--circuitparams.maxtxs = 0 \\/--circuitparams.maxtxs=0 \\/' $HOME/kroma-up/scripts/entrypoint.sh
 sed -i '/- kroma-geth/!b;n;/user: root/!a\    user: root' $HOME/kroma-up/docker-compose.yml
