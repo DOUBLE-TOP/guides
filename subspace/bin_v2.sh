33c32c9d9f5663f5eb0d5b44218b54fc6f59a845
razumv <razumv95@gmail.com>
2023-09-02 17:11:29 +0200

    Create bin_v2.sh

---
+++ subspace/bin_v2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -f /usr/local/bin/pulsar
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-v2-v0.6.5-alpha
+sudo chmod +x pulsar
+sudo mv pulsar /usr/local/bin/
