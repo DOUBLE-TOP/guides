eb55f122f8dced7dcacb1264124f9c1c4a4907b7
razumv <razumv95@gmail.com>
2023-09-06 00:48:58 +0200

    Update install.sh

--- fleek/install.sh
+++ fleek/install.sh
@@ -1,75 +1,63 @@
 #!/bin/bash
-
-function logo {
-  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh)
-}
-
-function line {
-  echo "-----------------------------------------------------------------------------"
-}
-
-function colors {
-  GREEN="\e[1m\e[32m"
-  RED="\e[1m\e[39m"
-  NORMAL="\e[0m"
-}
-
-function main_tools {
-  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
-}
-
-function rust {
+function install_rust {
+  sudo apt-get update
+  sudo apt-get install build-essential cmake clang pkg-config libssl-dev gcc-multilib protobuf-compiler -y
   bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh)
-  source $HOME/.profile
-  cargo install sccache
+  source ~/.cargo/env
+  sleep 1
 }
 
-function build_ursa {
-  cd $HOME
-  git clone https://github.com/fleek-network/ursa.git
-  cd ursa
-  make install
-  source $HOME/.profile
+function source_lightning {
+  git clone -b testnet-alpha-0 https://github.com/fleek-network/lightning.git
+  cd $HOME/lightning
+  cargo clean
+  cargo update
+  cargo build
 }
 
-function systemd_ursa {
-  sudo tee <<EOF >/dev/null /etc/systemd/system/fleek.service
+function symlink {
+  sudo ln -sf "$HOME/lightning/target/debug/lightning-node" /usr/local/bin/lgtn
+}
+
+function generate_keys {
+  lgtn keys generate
+}
+
+function config {
+  sed -i 's/testnet = .*/testnet = true/' $HOME/.lightning/config.toml
+}
+
+function systemd {
+  sudo tee <<EOF >/dev/null /etc/systemd/system/lgtn.service
 [Unit]
-Description=Fleek node
-After=network.target
+Description=Fleek Network Node lightning service
 
 [Service]
 User=$USER
-ExecStart=/root/.cargo/bin/ursa
-WorkingDirectory=$HOME/ursa
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
+Type=simple
+MemoryHigh=32G
+RestartSec=15s
+Restart=always
+ExecStart=lgtn -c $HOME/lightning/lightning.toml run
+StandardOutput=append:/var/log/lightning/output.log
+StandardError=append:/var/log/lightning/diagnostic.log
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-  sudo systemctl daemon-reload
-  sudo systemctl enable fleek &>/dev/null
-  sudo systemctl restart fleek
+sudo systemctl daemon-reload
+sudo systemctl enable lgtn
+sudo systemctl restart lgtn
 }
 
-colors
-line
-logo
-line
-echo "installing tools...."
-line
-main_tools
-rust
-line
-echo "clonning repository and build bin files"
-line
-build_ursa
-line
-echo "creating systemd file, adding to autostart, starting"
-systemd_ursa
-echo "installation complete, check logs by command:"
-echo "journalctl -n 100 -f -u fleek -o cat"
-echo "and wait for -bootstrap complete-"
+function main {
+  install_rust
+  source_lightning
+  symlink
+  generate_keys
+  config
+  systemd
+}
+
+main
