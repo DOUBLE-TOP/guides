f5229561a5d1cde50069fc59215d1cb5069449dc
razumv <razumv95@gmail.com>
2023-09-06 22:06:40 +0200

    0.6.6

--- subspace/install.sh
+++ subspace/install.sh
@@ -31,9 +31,9 @@ function wget_pulsar {
 
   # Выбор URL для скачивания на основе уровня
   if (( LEVEL >= 4 )); then
-      URL="https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.5-alpha"
+      URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.6-alpha"
   else
-      URL="https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-v2-v0.6.5-alpha"
+      URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-v2-v0.6.6-alpha"
   fi
   wget -O pulsar $URL
   sudo chmod +x pulsar
