cb34d5aeba64a3b839633df994f613e32f34810b
razumv <razumv95@gmail.com>
2023-09-14 12:07:53 +0200

    0.6.7

--- subspace/install.sh
+++ subspace/install.sh
@@ -30,10 +30,10 @@ function wget_pulsar {
   }')
 
   # Выбор URL для скачивания на основе уровня
-  if (( LEVEL >= 4 )); then
-      URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.6-alpha"
+  if (( LEVEL >= 3 )); then
+      URL="https://github.com/subspace/pulsar/releases/download/v0.6.7-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.7-alpha"
   else
-      URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-v2-v0.6.6-alpha"
+      URL="https://github.com/subspace/pulsar/releases/download/v0.6.7-alpha/pulsar-ubuntu-x86_64-v2-v0.6.7-alpha"
   fi
   wget -O pulsar $URL
   sudo chmod +x pulsar
--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -15,10 +15,10 @@ BEGIN {
 }')
 
 # Выбор URL для скачивания на основе уровня
-if (( LEVEL >= 4 )); then
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.6-alpha"
+if (( LEVEL >= 3 )); then
+    URL="https://github.com/subspace/pulsar/releases/download/v0.6.7-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.7-alpha"
 else
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.6-alpha/pulsar-ubuntu-x86_64-v2-v0.6.6-alpha"
+    URL="https://github.com/subspace/pulsar/releases/download/v0.6.7-alpha/pulsar-ubuntu-x86_64-v2-v0.6.7-alpha"
 fi
 
 # Удаляем старую версию, скачиваем новую и устанавливаем
