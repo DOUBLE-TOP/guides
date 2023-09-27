a918bd0bffc1574d30dac0700db0066dc287615b
razumv <razumv95@gmail.com>
2023-09-27 12:44:50 +0200

    Update bin_v2.sh

--- subspace/bin_v2.sh
+++ subspace/bin_v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f /usr/local/bin/pulsar
-wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.5-alpha/pulsar-ubuntu-x86_64-v2-v0.6.5-alpha
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.10-alpha/pulsar-ubuntu-x86_64-v2-v0.6.10-alpha
 sudo chmod +x pulsar
 sudo mv pulsar /usr/local/bin/
 sudo systemctl restart subspace
