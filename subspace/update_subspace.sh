d31662575ddc15a4c3acc2eaa38519d1049331d2
razumv <razumv95@gmail.com>
2023-10-10 15:00:14 +0200

    upd

--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -18,9 +18,9 @@ BEGIN {
 
 # Выбор URL для скачивания на основе уровня
 if (( LEVEL >= 3 )); then
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.13-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.13-alpha"
+    URL="https://github.com/subspace/pulsar/releases/download/v0.6.14-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.14-alpha"
 else
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.13-alpha/pulsar-ubuntu-x86_64-v2-v0.6.13-alpha"
+    URL="https://github.com/subspace/pulsar/releases/download/v0.6.14-alpha/pulsar-ubuntu-x86_64-v2-v0.6.14-alpha"
 fi
 
 # Удаляем старую версию, скачиваем новую и устанавливаем
