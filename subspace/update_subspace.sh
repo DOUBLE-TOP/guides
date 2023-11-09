d840bf1ef646adeaab81342bfb73ba7a136bddd9
razumv <razumv95@gmail.com>
2023-11-09 21:50:06 +0100

    update

--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-# Останавливаем сервис subspace
-sudo systemctl stop subspace
-(crontab -l ; echo "0 * * * * truncate -s 0 \$HOME/.local/share/pulsar/logs/*.log*") | crontab -
+docker-compose -f $HOME/subspace_docker/docker-compose.yml down
 
+sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2023-nov-09/g' $HOME/subspace_docker/docker-compose.yml
+sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2023-nov-09/g' $HOME/subspace_docker/docker-compose.yml
 
-# Определение уровня поддержки процессора. Спасибо за кусочек кода https://raw.githubusercontent.com/yurally/subspace/main/subspace_cli.sh
-LEVEL=$(awk '
-BEGIN {
-    while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1
-    if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1
-    if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2
-    if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3
-    if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4
-    print level; 
-}')
-
-# Выбор URL для скачивания на основе уровня
-if (( LEVEL >= 3 )); then
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.14-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.14-alpha"
-else
-    URL="https://github.com/subspace/pulsar/releases/download/v0.6.14-alpha/pulsar-ubuntu-x86_64-v2-v0.6.14-alpha"
-fi
-
-# Удаляем старую версию, скачиваем новую и устанавливаем
-rm -f /usr/local/bin/pulsar
-wget -O pulsar $URL
-sudo chmod +x pulsar
-sudo mv pulsar /usr/local/bin/
-
-# Перезапуск сервиса subspace
-sudo systemctl restart subspace
+docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d
