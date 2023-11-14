0c38bd3fad19cd697c8e6ec5c993097682433008
razumv <42905535+razumv@users.noreply.github.com>
2023-11-14 11:14:16 +0100

    Create ceremony.sh

---
+++ penumbra/ceremony.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pcli view sync
+pcli ceremony contribute --phase 1 --bid 60penumbra --coordinator-address penumbra1qvqr8cvqyf4pwrl6svw9kj8eypf3fuunrcs83m30zxh57y2ytk94gygmtq5k82cjdq9y3mlaa3fwctwpdjr6fxnwuzrsy4ezm0u2tqpzw0sed82shzcr42sju55en26mavjnw4
