8356e44c039d52c2f8dfa6672be844db6754e927
razumv <42905535+razumv@users.noreply.github.com>
2023-11-14 11:30:10 +0100

    Update ceremony.sh

--- penumbra/ceremony.sh
+++ penumbra/ceremony.sh
@@ -10,5 +10,6 @@ ceremony_bid=$(($current_balance-$dif))
 if [ $ceremony_bid -gt 0 ]; then
     pcli ceremony contribute --phase 1 --bid ${ceremony_bid}penumbra --coordinator-address penumbra1qvqr8cvqyf4pwrl6svw9kj8eypf3fuunrcs83m30zxh57y2ytk94gygmtq5k82cjdq9y3mlaa3fwctwpdjr6fxnwuzrsy4ezm0u2tqpzw0sed82shzcr42sju55en26mavjnw4
 else 
-    echo "Недостаточный баланс, получите токены с крана в дискорде и повторите попытку"
+    echo "ERROR: Недостаточный баланс, получите токены с крана в дискорде и повторите попытку"
+    exit 1
 fi
