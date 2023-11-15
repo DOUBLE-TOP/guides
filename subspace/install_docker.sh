7baa113e8c3fd543583c5a8e5dfc9e2f3d24251c
razumv <razumv95@gmail.com>
2023-11-15 21:38:33 +0100

    nov 15

--- subspace/install_docker.sh
+++ subspace/install_docker.sh
@@ -56,7 +56,7 @@ function plot_size {
 
 function get_vars {
   export CHAIN="gemini-3g"
-  export RELEASE="gemini-3g-2023-nov-14"
+  export RELEASE="gemini-3g-2023-nov-15"
 }
 
 function eof_docker_compose {
