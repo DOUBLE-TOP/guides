2e8239dbf4b6d99e39bb5e7d80f4480edcaa016d
razumv <42905535+razumv@users.noreply.github.com>
2023-11-17 10:23:41 +0100

    Update install.sh

--- frame/install.sh
+++ frame/install.sh
@@ -31,6 +31,10 @@ function prepare_files {
         if [ ! -d "$HOME/frame-validator/node-config" ]; then
             git clone https://github.com/frame-network/node-config.git
             sed -i 's|"url":.*|"url": "https://ethereum-sepolia.publicnode.com"|' node-config/testnet.json
+        else
+            rm -rf $HOME/frame-validator/node-config
+            git clone https://github.com/frame-network/node-config.git
+            sed -i 's|"url":.*|"url": "https://ethereum-sepolia.publicnode.com"|' node-config/testnet.json
         fi
     else
         echo -e "${YELLOW}Вероятно, нода на сервере уже была установлена ранее. Переходим на следующий шаг${NORMAL}"
@@ -45,7 +49,7 @@ function run_docker {
             echo -e "${YELLOW}Докер контейнер уже существует в статусе exited. Удаляем его и запускаем заново${NORMAL}"
             docker rm -f frame
         fi
-        docker run -d --name frame --rm -it -v $(pwd)/node-data:/home/user/.frame -v $(pwd)/node-config/testnet.json:/home/user/testnet.json public.ecr.aws/o8e2k8j7/nitro-node:frame --conf.file testnet.json
+        docker run -d --name frame --restart always -it -v $(pwd)/node-data:/home/user/.frame -v $(pwd)/node-config/testnet.json:/home/user/testnet.json public.ecr.aws/o8e2k8j7/nitro-node:frame --conf.file testnet.json
     fi
 
 }
