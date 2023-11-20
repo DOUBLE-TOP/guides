0c1ca6c938916a253a6c8b1ee28f2a49b1f8fa00
razumv <razumv95@gmail.com>
2023-11-20 23:07:37 +0100

    nov-20

--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -2,8 +2,8 @@
 
 docker-compose -f $HOME/subspace_docker/docker-compose.yml down
 
-sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2023-nov-19/g' $HOME/subspace_docker/docker-compose.yml
-sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2023-nov-19/g' $HOME/subspace_docker/docker-compose.yml
+sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2023-nov-20/g' $HOME/subspace_docker/docker-compose.yml
+sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2023-nov-20/g' $HOME/subspace_docker/docker-compose.yml
 
 docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d
 
