a38c6cd215d593f2fddfdeae4a224bf4fac2d3d7
razumv <42905535+razumv@users.noreply.github.com>
2023-12-18 13:57:52 +0100

    frame

--- frame/update.sh
+++ frame/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $HOME/frame-validator
 docker rm -f frame
 docker pull public.ecr.aws/o8e2k8j7/nitro-node:frame
-docker run -d --name frame --restart always -it --cpus="1.0" --memory="4g" -v $(pwd)/node-data:/home/user/.frame -v $(pwd)/node-config/testnet.json:/home/user/testnet.json public.ecr.aws/o8e2k8j7/nitro-node:frame --conf.file testnet.json
+docker run -d --name frame --restart always -it --cpus="1.0" -v $(pwd)/node-data:/home/user/.frame -v $(pwd)/node-config/testnet.json:/home/user/testnet.json public.ecr.aws/o8e2k8j7/nitro-node:frame --conf.file testnet.json
