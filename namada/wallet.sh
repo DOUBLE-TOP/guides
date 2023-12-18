1d2e1be103e5c6032ee33f8c67a022e4bcc37932
razumv <razumv95@gmail.com>
2023-12-18 23:15:12 +0100

    Update wallet.sh

--- namada/wallet.sh
+++ namada/wallet.sh
@@ -4,6 +4,15 @@ function go {
   bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh)
 }
 
+function rust {
+  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh)
+  source $HOME/.profile
+}
+
+function nodejs {
+  bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/node.sh)
+}
+
 function source_build_namada_latest {
     cd
     rm -rf namada
@@ -15,6 +24,8 @@ function source_build_namada_latest {
 
 function main {
     go
+    rust
+    nodejs
     source_build_namada_latest
 }
 
