674b02fb445ef7492de2d63b5c2fac1ef448b7f0
razumv <42905535+razumv@users.noreply.github.com>
2024-01-03 17:40:32 +0100

    upd subspace

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -5,8 +5,8 @@ FILE="$HOME/subspace_docker/docker-compose.yml"
 
 # Проверяем, существует ли файл
 if [ -f "$FILE" ]; then
-    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2023-dec-22/g' $HOME/subspace_docker/docker-compose.yml
-    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2023-dec-22/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2024-jan-03/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2024-jan-03/g' $HOME/subspace_docker/docker-compose.yml
     echo "Обновления были применены."
 else
     echo "Файл $FILE не существует."
@@ -14,17 +14,17 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3g-2023-dec-22   
-docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2023-dec-22
+docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-03   
+docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-03
 docker image prune -a -f
 
 cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout d504fed67492e5363b34308767d3281a0b9e21cf
+git checkout 196a0086f4240541cf3f48ee26db1c3251128d5a
 
-docker build -t ghcr.io/subspace/node:gemini-3g-2023-dec-22 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3g-2023-dec-22 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3g-2024-jan-03 -f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3g-2024-jan-03 -f Dockerfile-farmer .
 
 docker-compose -f $FILE up -d 
--- subspace/update_subspace.sh
+++ subspace/update_subspace.sh
@@ -2,8 +2,8 @@
 
 docker-compose -f $HOME/subspace_docker/docker-compose.yml down
 
-sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2023-dec-22/g' $HOME/subspace_docker*/docker-compose.yml
-sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2023-dec-22/g' $HOME/subspace_docker*/docker-compose.yml
+sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2024-jan-03/g' $HOME/subspace_docker*/docker-compose.yml
+sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2024-jan-03/g' $HOME/subspace_docker*/docker-compose.yml
 
 docker-compose -f $HOME/subspace_docker/docker-compose.yml up -d
 # docker-compose -f $HOME/subspace_docker_operator/docker-compose.yml up -d
