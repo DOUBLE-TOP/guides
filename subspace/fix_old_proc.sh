e7210f351fa3711d31f7f2dfa7efffbf86ecc5dc
razumv <42905535+razumv@users.noreply.github.com>
2024-01-29 17:11:54 +0100

    d81f1c91083580deb15a7e92c6c05210e26d7736

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -14,7 +14,7 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29   
+docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29-2  
 docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-29
 docker image prune -a -f
 
@@ -22,13 +22,13 @@ cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout 8bbdfbd7ca5d42652dcba2c81970d858cc5f05b9
+git checkout d81f1c91083580deb15a7e92c6c05210e26d7736
 
-docker build -t ghcr.io/subspace/node:gemini-3g-2024-jan-29 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3g-2024-jan-29 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3g-2024-jan-29-2-f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2-f Dockerfile-farmer .
 
 docker-compose -f $FILE up -d 
 
-# docker tag ghcr.io/subspace/node:gemini-3g-2024-jan-29 razumv95/node:gemini-3g-2024-jan-29
-# docker tag ghcr.io/subspace/farmer:gemini-3g-2024-jan-29 razumv95/farmer:gemini-3g-2024-jan-29
-# docker pull razumv95/node:gemini-3g-2024-jan-29 && docker pull razumv95/farmer:gemini-3g-2024-jan-29 && docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29 && docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-29 && docker tag razumv95/node:gemini-3g-2024-jan-29 ghcr.io/subspace/node:gemini-3g-2024-jan-29 && docker tag razumv95/farmer:gemini-3g-2024-jan-29 ghcr.io/subspace/farmer:gemini-3g-2024-jan-29 && docker-compose -f /home/user/subspace_docker/docker-compose.yml up -d 
+# docker tag ghcr.io/subspace/node:gemini-3g-2024-jan-29-2razumv95/node:gemini-3g-2024-jan-29
+# docker tag ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2razumv95/farmer:gemini-3g-2024-jan-29
+# docker pull razumv95/node:gemini-3g-2024-jan-29-2&& docker pull razumv95/farmer:gemini-3g-2024-jan-29-2&& docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29-2&& docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2&& docker tag razumv95/node:gemini-3g-2024-jan-29-2ghcr.io/subspace/node:gemini-3g-2024-jan-29-2&& docker tag razumv95/farmer:gemini-3g-2024-jan-29-2ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2&& docker-compose -f /home/user/subspace_docker/docker-compose.yml up -d 
