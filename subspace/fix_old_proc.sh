c67f31de84b447bc0668760106d8981c3d94abf0
razumv <42905535+razumv@users.noreply.github.com>
2024-01-31 12:07:30 +0100

    jan-31

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -5,8 +5,8 @@ FILE="$HOME/subspace_docker/docker-compose.yml"
 
 # Проверяем, существует ли файл
 if [ -f "$FILE" ]; then
-    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2024-jan-29/g' $HOME/subspace_docker/docker-compose.yml
-    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2024-jan-29/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3g-2024-jan-31/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3g-2024-jan-31/g' $HOME/subspace_docker/docker-compose.yml
     echo "Обновления были применены."
 else
     echo "Файл $FILE не существует."
@@ -14,21 +14,17 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29-2  
-docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-29
+docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-31  
+docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-31
 docker image prune -a -f
 
 cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout d81f1c91083580deb15a7e92c6c05210e26d7736
+git checkout 8ca4ca043b14264bc54a27f3e5dd0e7d904118bf
 
-docker build -t ghcr.io/subspace/node:gemini-3g-2024-jan-29-2 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3g-2024-jan-31 -f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3g-2024-jan-31 -f Dockerfile-farmer .
 
-docker-compose -f $FILE up -d 
-
-# docker tag ghcr.io/subspace/node:gemini-3g-2024-jan-29-2razumv95/node:gemini-3g-2024-jan-29
-# docker tag ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2razumv95/farmer:gemini-3g-2024-jan-29
-# docker pull razumv95/node:gemini-3g-2024-jan-29-2&& docker pull razumv95/farmer:gemini-3g-2024-jan-29-2&& docker rmi -f ghcr.io/subspace/node:gemini-3g-2024-jan-29-2&& docker rmi -f ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2&& docker tag razumv95/node:gemini-3g-2024-jan-29-2ghcr.io/subspace/node:gemini-3g-2024-jan-29-2&& docker tag razumv95/farmer:gemini-3g-2024-jan-29-2ghcr.io/subspace/farmer:gemini-3g-2024-jan-29-2&& docker-compose -f /home/user/subspace_docker/docker-compose.yml up -d 
+docker-compose -f $FILE up -d
--- subspace/install_docker.sh
+++ subspace/install_docker.sh
@@ -56,7 +56,7 @@ function plot_size {
 
 function get_vars {
   export CHAIN="gemini-3g"
-  export RELEASE="gemini-3g-2024-jan-29-2"
+  export RELEASE="gemini-3g-2024-jan-31"
 }
 
 function eof_docker_compose {
