1495b64e0683be9ca74c7309bda3aba28a5a0a4d
KURASH <92112412+kuraassh@users.noreply.github.com>
2024-02-02 16:47:30 +0100

    upd

--- boolnetwork/install.sh
+++ boolnetwork/install.sh
@@ -26,21 +26,23 @@ function get_nodename {
 
 function install_main_tools {
     echo -e "${YELLOW}Установка основных зависимостей:${NORMAL}"
-    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh) &>/dev/null
+    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh)
 }
 
 function install_rust {
-    echo -e "${YELLOW}Установка основных зависимостей:${NORMAL}"
-    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh) &>/dev/null
+    echo -e "${YELLOW}Установка rust:${NORMAL}"
+    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh)
 }
 
 function install_docker {
-    echo -e "${YELLOW}Установка основных зависимостей:${NORMAL}"
-    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh) &>/dev/null
+    echo -e "${YELLOW}Установка docker:${NORMAL}"
+    bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh)
 }
 
 function install {
     echo -e "${YELLOW}Запуск ноды boolnetwork в докере:${NORMAL}"
+    mkdir -p $HOME/.boolnetwork/
+    chown -R 1000:1000 .boolnetwork/
     docker run -d -v $HOME/.boolnetwork:/bool/.local/share/bnk-node --restart unless-stopped --name boolnetwork boolnetwork/bnk-node:release --validator --chain=tee --name $BOOL_NODENAME 
 }
 
