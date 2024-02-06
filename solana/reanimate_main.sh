e72443277ef39b6f791ca380d90f21c22427e901
KURASH <92112412+kuraassh@users.noreply.github.com>
2024-02-06 13:15:27 +0100

    Update reanimate_main.sh

--- solana/reanimate_main.sh
+++ solana/reanimate_main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-solana-install init v1.13.6
+solana-install init v1.17.20
 
 sudo systemctl stop solana
 
@@ -18,31 +18,35 @@ Restart=always
 RestartSec=1
 LimitNOFILE=2048000
 Environment=SOLANA_METRICS_CONFIG=host=https://metrics.solana.com:8086,db=mainnet-beta,u=mainnet-beta_write,p=password
-ExecStart=/root/.local/share/solana/install/active_release/bin/solana-validator \
---entrypoint 195.14.6.36:8001 \
---entrypoint 198.244.229.6:8001 \
---entrypoint 147.28.246.5:8001 \
---entrypoint 147.28.151.175:8001 \
---entrypoint 147.75.32.159:8001 \
+ExecStart=$HOME/.local/share/solana/install/active_release/bin/solana-validator \
+--entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+--entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
 --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
---known-validator GjbUdcvguSNUnEeiTWa8f6wC1r1szUtUwKiJCp4oxjFX \
---known-validator Cpcjz8kcLuxZLeTAxyKtCy1dtBb5kvYx35L1NBdmYKa7 \
---known-validator AhWZi767Wo6sDWhXwS8oftvVWhvZj6FEa3175CBJ7Mbj \
---known-validator 4z6YgEYt5U594UYfWh5dgYWEzpZLBqqfW9uQC7imzjne \
---known-validator EBk1bP8LUmoWBHbaBQcwo7AiPtz6dw4yfFb5di8idzny \
---known-validator Ch92wHj5wYDAXYT5BRgo3zwgeg5u6eLfiqZqbpw6Rpn5 \
+--known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+--known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+--known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+--known-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+--only-known-rpc \
 --no-snapshot-fetch \
+--no-genesis-fetch \
+--wait-for-supermajority 246464040 \
+--expected-shred-version 50093 \
+--expected-bank-hash 2QEvYhBgeWPJbC84fMTTK9NgntqiUAWiBEBf21rtTmng \
 --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --wal-recovery-mode skip_any_corrupted_record \
---identity /root/mainnet-validator-keypair.json \
---vote-account /root/vote-account-keypair.json \
---ledger /root/ledger \
+--identity $HOME/mainnet-validator-keypair.json \
+--vote-account $HOME/vote-account-keypair.json \
+--ledger $HOME/ledger \
 --limit-ledger-size 50000000 \
---dynamic-port-range 9030-9050 \
---log /root/solana.log \
+--dynamic-port-range 8000-8020 \
+--log $HOME/solana.log \
 --full-snapshot-interval-slots 25000 \
 --incremental-snapshot-interval-slots 500 \
+--maximum-full-snapshots-to-retain 1 \
+--maximum-incremental-snapshots-to-retain 2 \
+--maximum-local-snapshot-age 2500 \
 --rpc-port 8899 \
 --full-rpc-api \
 --private-rpc
@@ -50,15 +54,14 @@ ExecReload=/bin/kill -s HUP
 ExecStop=/bin/kill -s QUIT 
 [Install]
 WantedBy=multi-user.target
-
 EOF"
 
 sudo systemctl daemon-reload
 
-source $HOME/solana-snapshot-finder/venv/bin/activate
+#source $HOME/solana-snapshot-finder/venv/bin/activate
 
-python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger
+#python3 $HOME/solana-snapshot-finder/snapshot-finder.py --snapshot_path $HOME/ledger
 
-# wget -O $HOME/ledger/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst https://d1fe19ei6b1nmi.cloudfront.net/snapshot-179526403-FUdrVxEUnbp4AzZ5qNPgYB3viRQEGo2n1xq5n382Y8sC.tar.zst
+wget -O $HOME/ledger/snapshot-246464040-9kmqknr1D8pQXn92dWMDWvfZ9nwzgxRQ6JnbUrG3KN4F.tar.zst wget https://storage.googleapis.com/jito-mainnet/snapshot-246464040-9kmqknr1D8pQXn92dWMDWvfZ9nwzgxRQ6JnbUrG3KN4F.tar.zst
 
 sudo systemctl restart solana
