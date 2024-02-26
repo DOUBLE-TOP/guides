04214230682b4c71b22b77931ffde1655a53f076
razumv <42905535+razumv@users.noreply.github.com>
2024-02-26 12:31:51 +0100

    Update install.sh

--- dusk/install.sh
+++ dusk/install.sh
@@ -46,35 +46,6 @@ ENV RUST_BACKTRACE=full \\
 
 # Установка необходимых пакетов и выполнение всех операций одним слоем
 RUN apt update && apt install -y unzip curl jq net-tools logrotate dnsutils
-    # && mkdir -p /opt/dusk/bin \\
-    # && mkdir -p /opt/dusk/conf \\
-    # && mkdir -p /opt/dusk/rusk \\
-    # && mkdir -p /opt/dusk/services \\
-    # && mkdir -p /opt/dusk/installer \\
-    # && mkdir -p /root/.dusk/rusk-wallet \\
-    # && VERIFIER_KEYS_URL="https://nodes.dusk.network/keys" \\
-    # && LAST_STATE_URL="https://nodes.dusk.network/state/86920" \\
-    # && INSTALLER_URL="https://github.com/dusk-network/itn-installer/archive/refs/tags/v0.1.4.tar.gz" \\
-    # && WALLET_URL=$(curl -s "https://api.github.com/repos/dusk-network/wallet-cli/releases/latest" | jq -r  '.assets[].browser_download_url' | grep libssl3) \\
-    # && curl -so /opt/dusk/installer/installer.tar.gz -L "\$INSTALLER_URL" \\
-    # && tar xf /opt/dusk/installer/installer.tar.gz --strip-components=1 -C /opt/dusk/installer \\
-    # && mv /opt/dusk/installer/bin/* /opt/dusk/bin/ \\
-    # && mv /opt/dusk/installer/conf/* /opt/dusk/conf/ \\
-    # && mv /opt/dusk/installer/services/* /opt/dusk/services/ \\
-    # && chmod +x /opt/dusk/bin/* \\
-    # && ln -sf /opt/dusk/bin/rusk /usr/bin/rusk \\
-    # && curl -so /opt/dusk/installer/wallet.tar.gz -L "\$WALLET_URL" \\
-    # && mkdir -p /opt/dusk/installer/wallet \\
-    # && tar xf /opt/dusk/installer/wallet.tar.gz --strip-components 1 --directory /opt/dusk/installer/wallet \\
-    # && mv /opt/dusk/installer/wallet/rusk-wallet /opt/dusk/bin/ \\
-    # && chmod +x /opt/dusk/bin/rusk-wallet \\
-    # && ln -sf /opt/dusk/bin/rusk-wallet /usr/bin/rusk-wallet \\
-    # && mv /opt/dusk/conf/wallet.toml /root/.dusk/rusk-wallet/config.toml \\
-    # && curl -so /opt/dusk/installer/rusk-vd-keys.zip -L "\$VERIFIER_KEYS_URL" \\
-    # && unzip -o /opt/dusk/installer/rusk-vd-keys.zip -d /opt/dusk/rusk/ \\
-    # && curl -so /opt/dusk/installer/state.tar.gz -L "\$LAST_STATE_URL" \\
-    # && tar -xvf /opt/dusk/installer/state.tar.gz -C /opt/dusk/rusk/ \\
-    # && rm -rf /opt/dusk/installer
 
 COPY start.sh /start.sh
 
@@ -99,9 +70,6 @@ services:
       - "9900:9900"
       - "8980:8980"
 
-# volumes:
-#   rusk-data:
-#   rusk-wallet:
 EOF
 
 # Сборка контейнера
