0cd89ed1a5b337758d7c77b72b43fb6b0dea268e
razumv <42905535+razumv@users.noreply.github.com>
2024-02-28 14:19:43 +0100

    Update install.sh

--- dusk/install.sh
+++ dusk/install.sh
@@ -131,8 +131,8 @@ function start_dusk {
   docker-compose run dusk bash -c "/prestart.sh"
   docker-compose up -d
   sleep 15
-  docker-compose run dusk bash -c "/opt/dusk/bin/rusk-wallet --password \$DUSK_CONSENSUS_KEYS_PASS create --seed-file /opt/dusk/seed.txt"
-  docker-compose run dusk bash -c "/opt/dusk/bin/rusk-wallet --password \$DUSK_CONSENSUS_KEYS_PASS export -d /opt/dusk/conf -n consensus.keys"
+  docker exec -it rusk_dusk_1 bash -c "/opt/dusk/bin/rusk-wallet --state http://127.0.0.1:8980 --password \$DUSK_CONSENSUS_KEYS_PASS create --seed-file /opt/dusk/seed.txt"
+  docker exec -it rusk_dusk_1 bash -c "/opt/dusk/bin/rusk-wallet --state http://127.0.0.1:8980 --password \$DUSK_CONSENSUS_KEYS_PASS export -d /opt/dusk/conf -n consensus.keys"
 }
 
 function main {
