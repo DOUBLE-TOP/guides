e21b9fd38d842f0106911c667d97496a3aeb3e62
razumv <42905535+razumv@users.noreply.github.com>
2024-02-29 18:28:42 +0100

    Update update.sh

--- dusk/update.sh
+++ dusk/update.sh
@@ -27,11 +27,26 @@ function output_normal {
   echo -e "${GREEN}$1${NORMAL}"
 }
 
+function prepare_files {
+  cat > start.sh <<EOF
+#!/bin/bash
+
+# /opt/dusk/bin/rusk recovery-keys >> /var/log/rusk_recovery.log
+
+# /opt/dusk/bin/rusk recovery-state >> /var/log/rusk_recovery.log
+
+# /opt/dusk/bin/check_consensus_keys.sh
+
+exec /opt/dusk/bin/rusk --config /opt/dusk/conf/rusk.toml --kadcast-bootstrap bootstrap1.testnet.dusk.network:9000 --kadcast-bootstrap bootstrap2.testnet.dusk.network:9000 --http-listen-addr 0.0.0.0:8980 > /opt/dusk/rusk.log
+EOF
+}
 
 function update {
     cd $HOME/rusk
     docker-compose down
     cp -r dusk dusk-backup
+    docker rmi -f rusk-dusk:latest
+    docker-compose build
     docker-compose run dusk bash -c "curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/dusk/itn-installer.sh | bash"
     docker-compose up -d 
 }
@@ -42,6 +57,7 @@ function main {
     line
     output "Обновление Dusk Network"
     line
+    prepare_files
     update
     line
     output_normal "Обновление завершено"
