af8b5557643c46d1c60225a81c65082dfd770e4d
razumv <42905535+razumv@users.noreply.github.com>
2024-03-25 12:32:13 +0100

    Update install.sh

--- dusk/install.sh
+++ dusk/install.sh
@@ -71,7 +71,7 @@ DIR="/opt/dusk"
 if [ "\$(ls -A \$DIR)" ]; then
   echo "Cтартуем..."
 else
-  curl --proto "=https" --tlsv1.2 -sSfL https://github.com/dusk-network/itn-installer/releases/download/v0.1.8/itn-installer.sh | sh
+  curl --proto "=https" --tlsv1.2 -sSfL https://github.com/dusk-network/node-installer/releases/download/v0.2.0/node-installer.sh | sh
 fi
 EOF
 
