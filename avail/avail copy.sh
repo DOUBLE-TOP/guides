07cba8c2dde9d3ddcd9f7cea3d00c3172451c28a
razumv <42905535+razumv@users.noreply.github.com>
2024-04-03 12:17:08 +0200

    1

--- avail/avail copy.sh
+++ avail/avail copy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "рџ†™ Starting Availup..."
+index=0 # Установка значения по умолчанию для индекса.
+
 while [ $# -gt 0 ]; do
     if [[ $1 = "--index" ]]; then
         index="$2" # Изменение: присваиваем переменной index значение аргумента, следующего за --index.
@@ -14,20 +16,20 @@ while [ $# -gt 0 ]; do
     shift
 done
 # generate folders if missing
-if [ ! -d "$HOME/.avail" ]; then
-    mkdir $HOME/.avail
+if [ ! -d "$HOME/.avail$i" ]; then
+    mkdir $HOME/.avail$i
 fi
-if [ ! -d "$HOME/.avail/bin" ]; then
-    mkdir $HOME/.avail/bin
+if [ ! -d "$HOME/.avail$i/bin" ]; then
+    mkdir $HOME/.avail$i/bin
 fi
-if [ ! -d "$HOME/.avail/identity" ]; then
-    mkdir $HOME/.avail/identity
+if [ ! -d "$HOME/.avail$i/identity" ]; then
+    mkdir $HOME/.avail$i/identity
 fi
-if [ ! -d "$HOME/.avail/data" ]; then
-    mkdir $HOME/.avail/data
+if [ ! -d "$HOME/.avail$i/data" ]; then
+    mkdir $HOME/.avail$i/data
 fi
-if [ ! -d "$HOME/.avail/config" ]; then
-    mkdir $HOME/.avail/config
+if [ ! -d "$HOME/.avail$i/config" ]; then
+    mkdir $HOME/.avail$i/config
 fi
 # check if bash is current terminal shell, else check for zsh
 if [ -z "$BASH_VERSION" ]; then
@@ -67,13 +69,13 @@ if [ -z "$network" ]; then
 else
     NETWORK="$network"
 fi
-CONFIG_PARAMS="bootstraps=['/dns/bootnode.1.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWBkLsNGaD3SpMaRWtAmWVuiZg1afdNSPbtJ8M8r9ArGRT','/dns/bootnode.2.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWRCgfvaLSnQfkwGehrhSNpY7i5RenWKL2ARst6ZqgdZZd']\nfull_node_ws=['wss://rpc-goldberg.sandbox.avail.tools:443','wss://goldberg-rpc.fra.avail.tools:443']\nconfidence=99.0\navail_path='$HOME/.avail/data'\nkad_record_ttl=43200\not_collector_endpoint='http://otelcol.lightclient.goldberg.avail.tools:4317'\ngenesis_hash='6f09966420b2608d1947ccfb0f2a362450d1fc7fd902c29b67c906eaa965a7ae'\nhttp_server_port=700$index\nport=3700$index\n"
-AVAIL_BIN=$HOME/.avail/bin/avail-light
+CONFIG_PARAMS="bootstraps=['/dns/bootnode.1.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWBkLsNGaD3SpMaRWtAmWVuiZg1afdNSPbtJ8M8r9ArGRT','/dns/bootnode.2.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWRCgfvaLSnQfkwGehrhSNpY7i5RenWKL2ARst6ZqgdZZd']\nfull_node_ws=['wss://rpc-goldberg.sandbox.avail.tools:443','wss://goldberg-rpc.fra.avail.tools:443']\nconfidence=99.0\navail_path='$HOME/.avail$i/data'\nkad_record_ttl=43200\not_collector_endpoint='http://otelcol.lightclient.goldberg.avail.tools:4317'\ngenesis_hash='6f09966420b2608d1947ccfb0f2a362450d1fc7fd902c29b67c906eaa965a7ae'\nhttp_server_port=700$i\nport=3700$i\n"
+AVAIL_BIN=$HOME/.avail$i/bin/avail-light
 if [ "$NETWORK" = "goldberg" ]; then
     echo "рџ“Њ Goldberg testnet selected."
     VERSION="v1.7.10"
     if [ -z "$config" ]; then
-        CONFIG="$HOME/.avail/config/config.yml"
+        CONFIG="$HOME/.avail$i/config/config.yml"
         if [ -f "$CONFIG" ]; then
             echo "рџ—‘пёЏ  Wiping old config file at $CONFIG."
             rm $CONFIG
@@ -103,7 +105,7 @@ else
     APPID="$app_id"
 fi
 if [ -z "$identity" ]; then
-    IDENTITY=$HOME/.avail/identity/identity.toml
+    IDENTITY=$HOME/.avail$i/identity/identity.toml
     if [ -f "$IDENTITY" ]; then
         echo "рџ”‘ Identity found at $IDENTITY."
     else
@@ -117,7 +119,7 @@ UPGRADE=0
 if [ ! -z "$upgrade" ]; then
     echo "рџ”„ Checking for updates..."
     if [ -f $AVAIL_BIN ]; then
-        CURRENT_VERSION="v$($HOME/.avail/bin/avail-light --version | cut -d " " -f 2)"
+        CURRENT_VERSION="v$($HOME/.avail$i/bin/avail-light --version | cut -d " " -f 2)"
         if [ "$CURRENT_VERSION" = "v1.7.9" ]; then
             UPGRADE=1
             echo "в¬†пёЏ  Avail binary is out of date. Upgrading..."
@@ -133,7 +135,7 @@ if [ ! -z "$upgrade" ]; then
     fi
 else 
     if [ -f $AVAIL_BIN ]; then
-        CURRENT_VERSION="v$($HOME/.avail/bin/avail-light --version | cut -d " " -f 2)"
+        CURRENT_VERSION="v$($HOME/.avail$i/bin/avail-light --version | cut -d " " -f 2)"
         if [ "$CURRENT_VERSION" = "v1.7.9" ]; then
             UPGRADE=1
             echo "в¬†пёЏ  Avail binary is out of date. Upgrading..."
@@ -144,9 +146,9 @@ fi
 onexit() {
     chmod 600 $IDENTITY
     echo "рџ”„ Avail stopped. Future instances of the light client can be started by invoking the avail-light binary or rerunning this script$EXTRAPROMPT"
-    if [[ ":$PATH:" != *":$HOME/.avail/bin:"* ]]; then
-        if ! grep -q "export PATH=\"\$PATH:$HOME/.avail/bin\"" "$PROFILE"; then
-            echo -e "export PATH=\"\$PATH:$HOME/.avail/bin\"\n" >>$PROFILE
+    if [[ ":$PATH:" != *":$HOME/.avail$i/bin:"* ]]; then
+        if ! grep -q "export PATH=\"\$PATH:$HOME/.avail$i/bin\"" "$PROFILE"; then
+            echo -e "export PATH=\"\$PATH:$HOME/.avail$i/bin\"\n" >>$PROFILE
         fi
         echo -e "рџ“Њ Avail has been added to your profile. Run the following command to load it in the current session:\n. $PROFILE\n"
     fi
@@ -168,9 +170,9 @@ if [ "$UPGRADE" = 1 ]; then
             touch $CONFIG
             echo -e $CONFIG_PARAMS >>$CONFIG
         fi
-        if [ -d "$HOME/.avail/data" ]; then
-            rm -rf $HOME/.avail/data
-            mkdir $HOME/.avail/data
+        if [ -d "$HOME/.avail$i/data" ]; then
+            rm -rf $HOME/.avail$i/data
+            mkdir $HOME/.avail$i/data
         fi
     else
         echo "рџ¤” Avail was not installed with availup. Attemping to uninstall with cargo..."
