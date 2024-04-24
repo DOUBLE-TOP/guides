7ea8a425c4641f7fb19148e00ff1344bea533667
razumv <42905535+razumv@users.noreply.github.com>
2024-04-24 15:30:09 +0200

    17273eeefd6781f4fa570e42cac722e580f9476a

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -5,8 +5,8 @@ FILE="$HOME/subspace_docker/docker-compose.yml"
 
 # Проверяем, существует ли файл
 if [ -f "$FILE" ]; then
-    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3h-2024-mar-29/g' $HOME/subspace_docker/docker-compose.yml
-    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3h-2024-mar-29/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/subspace\/node:gemini-3h-2024-apr-24/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/subspace\/farmer:gemini-3h-2024-apr-24/g' $HOME/subspace_docker/docker-compose.yml
     echo "Обновления были применены."
 else
     echo "Файл $FILE не существует."
@@ -14,17 +14,17 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-mar-29
-docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-mar-29
+docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-apr-24
+docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-apr-24
 docker image prune -a -f
 
 cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout 8d18af2734183e018b57dcf001611d7fcc074b81
+git checkout 17273eeefd6781f4fa570e42cac722e580f9476a
 
-docker build -t ghcr.io/subspace/node:gemini-3h-2024-mar-29 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-mar-29 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3h-2024-apr-24 -f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-apr-24 -f Dockerfile-farmer .
 
 docker-compose -f $FILE up -d
