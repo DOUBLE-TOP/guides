4d5096b114b0e303c247ea322f24e6caebb671fd
razumv <42905535+razumv@users.noreply.github.com>
2024-05-15 19:48:59 +0200

    Update install.sh

--- initia/install.sh
+++ initia/install.sh
@@ -105,7 +105,7 @@ function config_node {
 
     sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.initia/config/config.toml
     sed -i -e "s|^node *=.*|node = \"tcp://localhost:14657\"|" $HOME/.initia/config/client.toml
-    sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit,0.01uusdc\"|" $HOME/.initia/config/app.toml
+    sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.15uinit\"|" $HOME/.initia/config/app.toml
 
 }
 
