a8508ad9972a9740f44f3e40d2980a33de8d9ba0
razumv <42905535+razumv@users.noreply.github.com>
2024-05-24 10:33:27 +0200

    b27d113cc7805a3ab106bac299c2bfb23207ec14

--- subspace/install_docker.sh
+++ subspace/install_docker.sh
@@ -56,7 +56,7 @@ function plot_size {
 
 function get_vars {
   export CHAIN="gemini-3g"
-  export RELEASE="gemini-3h-2024-may-15"
+  export RELEASE="gemini-3h-2024-may-24"
 }
 
 function eof_docker_compose {
