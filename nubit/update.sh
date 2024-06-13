7dc39d89b902064fe2da0a541f6af81a35558190
ivan.pohosov <pogosovivan@gmail.com>
2024-06-14 00:50:52 +0300

    Update nubit node

---
+++ nubit/update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+sudo apt update && sudo apt upgrade -y && apt install curl -y
+
+echo "-----------------------------------------------------------------------------"
+echo "Сохранение данных с ноды Nubit"
+echo "-----------------------------------------------------------------------------"
+
+docker cp nubit:/home/nubit-user/.nubit-light-nubit-alphatestnet-1 $HOME/nubit-node
+
+echo "-----------------------------------------------------------------------------"
+echo "Удаление старых данных с ноды Nubit"
+echo "-----------------------------------------------------------------------------"
+
+cd $HOME/nubit-node
+rm -rf Dockerfile
+
+docker rm -f nubit
+docker images | grep "nubit" | awk '{print $3}' | xargs docker rmi -f
+docker system prune -af
+
+echo "-----------------------------------------------------------------------------"
+echo "Обновление ноды Nubit"
+echo "-----------------------------------------------------------------------------"
+
+wget https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/nubit/Dockerfile
+docker build -t nubit_image . && docker run -d --name nubit nubit_image
+
+sleep 60
+docker cp nubit:/home/nubit-user/nubit-node/mnemonic.txt $HOME/nubit-node/mnemonic.txt
+docker cp nubit:/home/nubit-user/.nubit-light-nubit-alphatestnet-1/keys $HOME/nubit-node/keys
+
+echo "-----------------------------------------------------------------------------"
+echo "Light Nubit Node успешно установлена"
+echo "-----------------------------------------------------------------------------"
+echo "Mnemonic"
+echo "cat $HOME/nubit-node/mnemonic.txt"
+echo "-----------------------------------------------------------------------------"
+echo "Backup Keys from folder"
+echo "$HOME/nubit-node/keys"
+echo "-----------------------------------------------------------------------------"
+echo "Проверка логов:"
+echo "docker logs -f --tail=100 nubit"
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
