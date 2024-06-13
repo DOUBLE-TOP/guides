5389c628123596f9e006a76847052f6d88991dc8
razumv <42905535+razumv@users.noreply.github.com>
2024-06-13 11:47:25 +0200

    Update update_storage.sh

--- 0g/update_storage.sh
+++ 0g/update_storage.sh
@@ -3,8 +3,11 @@
 source $HOME/.profile
 
 cd $HOME/0g-storage-node/
+git checkout -- run/config.toml
 git fetch --all --tags
 git checkout tags/$1
 sudo systemctl stop 0g_storage
 cargo build --release
+latest_block=$($HOME/go/bin/0gchaind status | jq -r .sync_info.latest_block_height)
+sed -i 's/log_sync_start_block_number = [0-9]\+/log_sync_start_block_number = '"$latest_block"'/g' run/config.toml
 sudo systemctl restart 0g_storage
