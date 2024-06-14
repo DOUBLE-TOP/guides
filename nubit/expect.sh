d3074b0e25ae70f9c6f28c0d94ab12b50878ff10
ivan.pohosov <pogosovivan@gmail.com>
2024-06-14 18:14:27 +0300

    Update expect file

--- nubit/expect.sh
+++ nubit/expect.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/expect -f
+export text_file="/home/nubit-user/mnemonic.txt"
 spawn /home/nubit-user/nubit-node/bin/nkey add my_keplr_key --recover --keyring-backend test --node.type light --p2p.network nubit-alphatestnet-1
 expect "Enter your bip39 mnemonic"
-send "/home/nubit-user/mnemonic.txt\r"
+send -- "$text_file\r"
 interact
