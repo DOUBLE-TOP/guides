e843ab57a0f87c63422c946d796fe97962ad39c8
ivan.pohosov <pogosovivan@gmail.com>
2024-06-16 19:44:52 +0300

    Update expect for nubit

--- nubit/expect.sh
+++ nubit/expect.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn /home/nubit-user/nubit-node/bin/nkey add my_keplr_key --recover --keyring-backend test --node.type light --p2p.network nubit-alphatestnet-1
+spawn /home/nubit-user/nubit-node/bin/nkey add my_nubit_key --recover --keyring-backend test --node.type light --p2p.network nubit-alphatestnet-1
 expect "Enter your bip39 mnemonic"
 send "MNEMONIC\r"
 interact
