5552c3ed58786a45955ebf9d649cbb4e3b4b6599
KURASH <92112412+kuraassh@users.noreply.github.com>
2024-06-27 20:08:11 +0200

    Update install.sh

--- 0g/install.sh
+++ 0g/install.sh
@@ -23,17 +23,17 @@ echo "--------------------------------------------------------------------------
 cd $HOME
 git clone https://github.com/0glabs/0g-chain.git
 cd 0g-chain
-git checkout v0.1.0
+git checkout v0.2.3 
 make install
 0gchaind version
 echo "Репозиторий успешно склонирован, начинаем билд"
 echo "-----------------------------------------------------------------------------"
-0gchaind config chain-id zgtendermint_16600-1
+0gchaind config chain-id zgtendermint_16600-2
 0gchaind config keyring-backend file
-0gchaind init $OG_NODENAME --chain-id zgtendermint_9000-1 &>/dev/null
+0gchaind init $OG_NODENAME --chain-id zgtendermint_16600-2 &>/dev/null
 0gchaind config node tcp://127.0.0.1:12657
-wget https://github.com/0glabs/0g-chain/releases/download/v0.1.0/genesis.json -O $HOME/.0gchain/config/genesis.json
-curl -Ls https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json > $HOME/.0gchain/config/addrbook.json
+wget https://github.com/0glabs/0g-chain/releases/download/v0.2.3/genesis.json -O $HOME/.0gchain/config/genesis.json
+#curl -Ls https://snapshots.liveraven.net/snapshots/testnet/zero-gravity/addrbook.json > $HOME/.0gchain/config/addrbook.json
 
 EXTERNAL_IP=$(wget -qO- eth0.me) \
 PROXY_APP_PORT=12658 \
@@ -55,7 +55,7 @@ sed -i \
     -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
     -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
     -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
-SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+SEEDS="81987895a11f6689ada254c6b57932ab7ed909b6@54.241.167.190:26656,010fb4de28667725a4fef26cdc7f9452cc34b16d@54.176.175.48:26656,e9b4bc203197b62cc7e6a80a64742e752f4210d5@54.193.250.204:26656,68b9145889e7576b652ca68d985826abd46ad660@18.166.164.232:26656" && \
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.0gchain/config/config.toml
 
 
@@ -70,11 +70,11 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.00252ua0gi\"/" $HOME/.0gchain/config/app.toml
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.0gchain/config/config.toml
 
-echo "Билд закончен, скачиваем снепшот"
-echo "-----------------------------------------------------------------------------"
-curl https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4 | sudo lz4 -dc - | sudo tar -xf - -C $HOME/.0gchain
+#echo "Билд закончен, скачиваем снепшот"
+#echo "-----------------------------------------------------------------------------"
+#curl https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4 | sudo lz4 -dc - | sudo tar -xf - -C $HOME/.0gchain
 
-echo "Снепшот скачан, переходим к инициализации ноды"
+echo "Переходим к инициализации ноды"
 echo "-----------------------------------------------------------------------------"
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
@@ -101,9 +101,9 @@ sudo systemctl restart 0g
 
 echo "Validator Node $OG_NODENAME успешно установлена"
 echo "-----------------------------------------------------------------------------"
-echo "Проверяем синхронизацию Validator Node $OG_NODENAME"
-echo "-----------------------------------------------------------------------------"
-bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/0g/check.sh)
-echo "-----------------------------------------------------------------------------"
+#echo "Проверяем синхронизацию Validator Node $OG_NODENAME"
+#echo "-----------------------------------------------------------------------------"
+#bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/0g/check.sh)
+#echo "-----------------------------------------------------------------------------"
 echo "Wish lifechange case with DOUBLETOP"
 echo "-----------------------------------------------------------------------------"
