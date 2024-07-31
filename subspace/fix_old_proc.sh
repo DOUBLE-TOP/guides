bebbb19c81d6d5bf3038d54d4964c0988bab5fbb
muller <mdb.crypto2021@gmail.com>
2024-07-31 13:00:26 +0300

    gemini-3h-2024-jul-29

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -5,8 +5,8 @@ FILE="$HOME/subspace_docker/docker-compose.yml"
 
 # Проверяем, существует ли файл
 if [ -f "$FILE" ]; then
-    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/autonomys\/node:gemini-3h-2024-jul-26/g' $HOME/subspace_docker/docker-compose.yml
-    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/autonomys\/farmer:gemini-3h-2024-jul-26/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/autonomys\/node:gemini-3h-2024-jul-29/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/autonomys\/farmer:gemini-3h-2024-jul-29/g' $HOME/subspace_docker/docker-compose.yml
     sed -i 's/--state-pruning", "archive-canonical"/--state-pruning", "140000"/g' $HOME/subspace_docker*/docker-compose.yml
     echo "Обновления были применены."
 else
@@ -15,17 +15,17 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-jul-26
-docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-jul-26
+docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-jul-29
+docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-jul-29
 docker image prune -a -f
 
 cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout 0a89bdbeb480ae4446d38b3c06c720f9021858f6
+git checkout 4de5f0534c6fc377d350b7c97ddaed638fd51fe4
 
-docker build -t ghcr.io/subspace/node:gemini-3h-2024-jul-26 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-jul-26 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3h-2024-jul-29 -f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-jul-29 -f Dockerfile-farmer .
 
 docker-compose -f $FILE up -d
--- subspace/install_docker.sh
+++ subspace/install_docker.sh
@@ -56,7 +56,7 @@ function plot_size {
 
 function get_vars {
   export CHAIN="gemini-3h"
-  export RELEASE="gemini-3h-2024-jul-26"
+  export RELEASE="gemini-3h-2024-jul-29"
 }
 
 function eof_docker_compose {
