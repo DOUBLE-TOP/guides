148f946d46071974e4d3887a7dd4b22c09bd5101
muller <mdb.crypto2021@gmail.com>
2024-08-01 15:09:46 +0300

    wget archive 0.2.5

---
+++ 0g/install_validator_archive.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+if [ ! $OG_NODENAME ]; then
+        read -p "Введите ваше имя ноды(придумайте, без спецсимволов - только буквы и цифры): " OG_NODENAME
+fi
+sleep 1
+echo 'export OG_NODENAME='$OG_NODENAME >> $HOME/.profile
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем софт"
+echo "-----------------------------------------------------------------------------"
+sudo apt update && sudo apt upgrade -y
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh | bash &>/dev/null
+sudo apt install --fix-broken -y &>/dev/null
+sudo apt install nano mc wget build-essential git jq make gcc tmux chrony lz4 unzip ncdu htop -y &>/dev/null
+source .profile
+source .bashrc
+sleep 1
+echo "Весь необходимый софт установлен"
+echo "-----------------------------------------------------------------------------"
+cd $HOME
+wget https://github.com/0glabs/0g-chain/archive/refs/tags/v0.2.5.zip
+unzip v0.2.5.zip && rm -rf v0.2.5.zip
+mv 0g-chain-0.2.5/ 0g-chain/
+cd $HOME/0g-chain
+make install
+echo "Репозиторий успешно склонирован, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+0gchaind config chain-id zgtendermint_16600-2
+0gchaind config keyring-backend file
+0gchaind init $OG_NODENAME --chain-id zgtendermint_16600-2 &>/dev/null
+0gchaind config node tcp://127.0.0.1:12657
+wget https://github.com/0glabs/0g-chain/releases/download/v0.2.3/genesis.json -O $HOME/.0gchain/config/genesis.json
+curl -s https://snapshots-testnet.nodejumper.io/0g-testnet/addrbook.json > $HOME/.0gchain/config/addrbook.json
+
+EXTERNAL_IP=$(wget -qO- eth0.me) \
+PROXY_APP_PORT=12658 \
+P2P_PORT=12656 \
+PPROF_PORT=12060 \
+API_PORT=12317 \
+GRPC_PORT=12090 \
+RPC_PORT=12657 \
+GRPC_WEB_PORT=12091
+
+sed -i \
+    -e "s/\(proxy_app = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$PROXY_APP_PORT\"/" \
+    -e "s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$RPC_PORT\"/" \
+    -e "s/\(pprof_laddr = \"\)\([^:]*\):\([0-9]*\).*/\1localhost:$PPROF_PORT\"/" \
+    -e "/\[p2p\]/,/^\[/{s/\(laddr = \"tcp:\/\/\)\([^:]*\):\([0-9]*\).*/\1\2:$P2P_PORT\"/}" \
+    -e "/\[p2p\]/,/^\[/{s/\(external_address = \"\)\([^:]*\):\([0-9]*\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/; t; s/\(external_address = \"\).*/\1${EXTERNAL_IP}:$P2P_PORT\"/}" \
+    $HOME/.0gchain/config/config.toml
+sed -i \
+    -e "/\[api\]/,/^\[/{s/\(address = \"tcp:\/\/\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$API_PORT\4/}" \
+    -e "/\[grpc\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_PORT\4/}" \
+    -e "/\[grpc-web\]/,/^\[/{s/\(address = \"\)\([^:]*\):\([0-9]*\)\(\".*\)/\1\2:$GRPC_WEB_PORT\4/}" $HOME/.0gchain/config/app.toml
+SEEDS="81987895a11f6689ada254c6b57932ab7ed909b6@54.241.167.190:26656,010fb4de28667725a4fef26cdc7f9452cc34b16d@54.176.175.48:26656,e9b4bc203197b62cc7e6a80a64742e752f4210d5@54.193.250.204:26656,68b9145889e7576b652ca68d985826abd46ad660@18.166.164.232:26656" && \
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.0gchain/config/config.toml
+sed -i 's/logs-cap = 10000/logs-cap = 100000/' $HOME/.0gchain/config/app.toml
+sed -i 's/block-range-cap = 10000/block-range-cap = 100000/' $HOME/.0gchain/config/app.toml
+
+
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.0gchain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.0gchain/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.0gchain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.0gchain/config/app.toml
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.00252ua0gi\"/" $HOME/.0gchain/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.0gchain/config/config.toml
+
+#echo "Билд закончен, скачиваем снепшот"
+#echo "-----------------------------------------------------------------------------"
+#curl https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4 | sudo lz4 -dc - | sudo tar -xf - -C $HOME/.0gchain
+
+echo "Переходим к инициализации ноды"
+echo "-----------------------------------------------------------------------------"
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+sudo systemctl restart systemd-journald
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/0g.service
+[Unit]
+  Description=OG Cosmos daemon
+  After=network-online.target
+[Service]
+  User=$USER
+  ExecStart=$(which 0gchaind) start --home $HOME/.0gchain
+  Restart=on-failure
+  RestartSec=10
+  LimitNOFILE=65535
+[Install]
+  WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable 0g &>/dev/null
+sudo systemctl daemon-reload
+sudo systemctl restart 0g
+
+echo "Validator Node $OG_NODENAME успешно установлена"
+echo "-----------------------------------------------------------------------------"
+#echo "Проверяем синхронизацию Validator Node $OG_NODENAME"
+#echo "-----------------------------------------------------------------------------"
+#bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/0g/check.sh)
+#echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
