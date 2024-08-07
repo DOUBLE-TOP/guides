f60142ad8cddd9255076c94a7edcafe22ce27324
ivan.pohosov <pogosovivan@gmail.com>
2024-08-07 11:22:24 +0300

    Chasm update

---
+++ chasm-network/update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+function colors {
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+function logo {
+  curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+}
+
+function line {
+  echo -e "${GREEN}-----------------------------------------------------------------------------${NORMAL}"
+}
+
+
+function run_docker {
+    echo -e "${YELLOW}Запускаем докер контейнер для валидатора${NORMAL}"
+    docker pull chasmtech/chasm-scout:latest
+    docker rm -f scout
+    cd $HOME/chasm-network
+    docker run -d --restart=always --env-file ./.env -p 3001:3001 --name scout chasmtech/chasm-scout
+}
+
+function output {
+    echo -e "${YELLOW}Для проверки логов выполняем команду:${NORMAL}"
+    echo -e "docker logs -f scout --tail=100"
+    echo -e "${YELLOW}Для перезапуска выполняем команду:${NORMAL}"
+    echo -e "docker restart scout"
+}
+
+colors
+line
+logo
+line
+run_docker
+line
+output
+line
+echo "Wish lifechange case with DOUBLETOP"
+line
