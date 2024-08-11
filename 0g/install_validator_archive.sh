c6f26baaf2e39ad45164211edd839f2267a1bb6e
muller <mdb.crypto2021@gmail.com>
2024-08-11 21:39:35 +0300

    0.3.1

--- 0g/install_validator_archive.sh
+++ 0g/install_validator_archive.sh
@@ -21,9 +21,9 @@ sleep 1
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
 cd $HOME
-wget https://github.com/0glabs/0g-chain/archive/refs/tags/v0.3.1.alpha.1.zip
-unzip 0.3.1.alpha.1.zip && rm -rf 0.3.1.alpha.1.zip
-mv 0g-chain-0.3.1.alpha.1/ 0g-chain/
+wget https://github.com/0glabs/0g-chain/archive/refs/tags/v0.3.1.zip
+unzip 0.3.1.zip && rm -rf 0.3.1.zip
+mv 0g-chain-0.3.1/ 0g-chain/
 cd $HOME/0g-chain
 make install
 echo "Репозиторий успешно склонирован, начинаем билд"
