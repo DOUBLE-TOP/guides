d86abe7f7b846c924c5540f7506a37836ea40d7f
muller <mdb.crypto2021@gmail.com>
2024-08-12 16:51:17 +0300

    upd snap

--- 0g/snapshot1.sh
+++ 0g/snapshot1.sh
@@ -4,7 +4,7 @@ sudo apt install lz4 -y
 sudo systemctl stop 0g
 cp $HOME/.0gchain/data/priv_validator_state.json $HOME/.0gchain/priv_validator_state.json.backup
 0gchaind tendermint unsafe-reset-all --home $HOME/.0gchain --keep-addr-book
-curl -L http://37.120.189.81/0g_testnet/0g_snap.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.0gchain
+curl https://server-5.itrocket.net/testnet/og/og_2024-08-12_628002_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.0gchain
 mv $HOME/.0gchain/priv_validator_state.json.backup $HOME/.0gchain/data/priv_validator_state.json
 sudo systemctl restart 0g
-rm -f 0g_snap.tar.lz4
+rm -f og_2024-08-12_628002_snap.tar.lz4
