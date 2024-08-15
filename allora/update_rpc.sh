cd78c01ff9473f507ccbd61fc54757336bba839b
ivan.pohosov <pogosovivan@gmail.com>
2024-08-15 13:08:36 +0300

    Update allora rpc

---
+++ allora/update_rpc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+echo "-----------------------------------------------------------------------------"
+echo "Обновление Allora RPC"
+echo "-----------------------------------------------------------------------------"
+
+cd basic-coin-prediction-node
+docker compose down -v
+
+sed -i "s|sentries|allora|" $HOME/basic-coin-prediction-node/model.py
+
+chmod +x init.config
+./init.config
+
+docker compose up -d
+
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
