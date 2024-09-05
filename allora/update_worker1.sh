20c139b18d05842ec9cc13d74417ad90809c0317
ivan.pohosov <pogosovivan@gmail.com>
2024-09-05 17:53:05 +0300

    Update aloora worker installer1

--- allora/update_worker1.sh
+++ allora/update_worker1.sh
@@ -8,7 +8,19 @@ echo "--------------------------------------------------------------------------
 echo "Обновление Allora Worker - Basic Coin Prediction Node"
 echo "-----------------------------------------------------------------------------"
 
-source .profile
+source $HOME/.profile
+
+if [ -z "${!ALLORA_SEED_PHRASE}" ]; then
+    echo "Введите сид фразу от кошелька, который будет использоваться для воркера"
+    read ALLORA_SEED_PHRASE
+    echo 'export ALLORA_SEED_PHRASE='$ALLORA_SEED_PHRASE >> $HOME/.profile
+fi
+
+if [ -z "${!COIN_GECKO_API_KEY}" ]; then
+    echo "Введите Coin Gecko API key"
+    read COIN_GECKO_API_KEY
+    echo 'export COIN_GECKO_API_KEY='$COIN_GECKO_API_KEY >> $HOME/.profile
+fi
 
 cd basic-coin-prediction-node
 docker compose down -v
@@ -21,13 +33,26 @@ cd basic-coin-prediction-node
 rm -rf config.json
 
 wget https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/allora/config.json
-sed -i "s|SeedPhrase|$WALLET_SEED_PHRASE|" $HOME/basic-coin-prediction-node/config.json
+sed -i "s|SeedPhrase|$ALLORA_SEED_PHRASE|" $HOME/basic-coin-prediction-node/config.json
 
 chmod +x init.config
 ./init.config
 
 sed -i "s|\"8000:8000|\"18000:8000|" $HOME/basic-coin-prediction-node/docker-compose.yml
 sed -i "s|intervals = [\"1d\"]|intervals = [\"10m\", \"20m\", \"1h\", \"1d\"]|" $HOME/basic-coin-prediction-node/model.py
+
+sudo tee $HOME/basic-coin-prediction-nod/.env > /dev/null <<EOF
+TOKEN=ETH
+TRAINING_DAYS=30
+TIMEFRAME=4h
+MODEL=LinearRegression
+REGION=EU
+DATA_PROVIDER=Binance
+CG_API_KEY=$COIN_GECKO_API_KEY
+EOF
+
+
+
 
 docker compose up -d --build
 
