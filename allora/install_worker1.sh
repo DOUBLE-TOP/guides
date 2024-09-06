09bde4d8fbd8b5828b269883e2dd13afed0b8f3c
ivan.pohosov <pogosovivan@gmail.com>
2024-09-06 11:43:29 +0300

    Update allora worker1 installer

--- allora/install_worker1.sh
+++ allora/install_worker1.sh
@@ -5,11 +5,16 @@ curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | b
 echo "-----------------------------------------------------------------------------"
 
 echo "-----------------------------------------------------------------------------"
-echo "Установка Allora Worker"
+echo "Установка Allora Worker - Basic Coin Prediction Node"
 echo "-----------------------------------------------------------------------------"
 
-echo "Введите сид фразу от кошелька, который будет использоваться для воркера"
-read WALLET_SEED_PHRASE
+source $HOME/.profile
+
+if [ -z "$ALLORA_SEED_PHRASE" ]; then
+    echo "Введите сид фразу от кошелька, который будет использоваться для воркера"
+    read ALLORA_SEED_PHRASE
+    echo "export ALLORA_SEED_PHRASE='$ALLORA_SEED_PHRASE'" >> $HOME/.profile
+fi
 
 cd $HOME
 git clone https://github.com/allora-network/basic-coin-prediction-node
@@ -17,15 +22,25 @@ cd basic-coin-prediction-node
 rm -rf config.json
 
 wget https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/allora/config.json
-sed -i "s|SeedPhrase|$WALLET_SEED_PHRASE|" $HOME/basic-coin-prediction-node/config.json
-sed -i "s|\":8000|\":18000|" $HOME/basic-coin-prediction-node/config.json
-
+sed -i "s|SeedPhrase|$ALLORA_SEED_PHRASE|" $HOME/basic-coin-prediction-node/config.json
 
 chmod +x init.config
 ./init.config
 
-sed -i "s|\"8000:8000\"|\"18000:8000\"|" $HOME/basic-coin-prediction-node/docker-compose.yml
-sed -i "s|intervals = [\"1d\"]|intervals = [\"10m\", \"1d\", \"10m\", \"1d\", \"10m\", \"1d\", \"20m\", \"20m\", \"20m\"]|" $HOME/basic-coin-prediction-node/model.py
+sed -i "s|\"8000:8000|\"18000:8000|" $HOME/basic-coin-prediction-node/docker-compose.yml
+sed -i "s|intervals = [\"1d\"]|intervals = [\"10m\", \"20m\", \"1h\", \"1d\"]|" $HOME/basic-coin-prediction-node/model.py
+
+sudo tee $HOME/basic-coin-prediction-node/.env > /dev/null <<EOF
+TOKEN=ETH
+TRAINING_DAYS=30
+TIMEFRAME=4h
+MODEL=LinearRegression
+REGION=EU
+DATA_PROVIDER=Binance
+CG_API_KEY=
+EOF
+
+sleep 5
 
 docker compose up -d --build
 
