f533f577bfbae890e84e31e3f047455e68f90017
ivan.pohosov <pogosovivan@gmail.com>
2024-10-03 23:24:35 +0300

    Add go for hemi

--- heminetwork/install.sh
+++ heminetwork/install.sh
@@ -6,6 +6,7 @@ echo "--------------------------------------------------------------------------
 
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh | bash &>/dev/null
 
 echo "-----------------------------------------------------------------------------"
 echo "Установка майнера Hemi Network"
