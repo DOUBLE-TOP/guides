31948e343448f6215dce3abdfe1da5374068c3e3
ivan.pohosov <pogosovivan@gmail.com>
2024-10-09 00:13:32 +0300

    Fix hemi update

--- heminetwork/install.sh
+++ heminetwork/install.sh
@@ -15,7 +15,8 @@ echo "--------------------------------------------------------------------------
 wget https://github.com/hemilabs/heminetwork/releases/download/v0.4.4/heminetwork_v0.4.4_linux_amd64.tar.gz
 
 tar -xvf heminetwork_v0.4.4_linux_amd64.tar.gz && rm heminetwork_v0.4.4_linux_amd64.tar.gz
-mv $HOME/heminetwork_v0.4.4_linux_amd64 $HOME/heminetwork
+mv -f $HOME/heminetwork_v0.4.4_linux_amd64/* $HOME/heminetwork
+rm -rf $HOME/heminetwork_v0.4.4_linux_amd64
 
 echo "-----------------------------------------------------------------------------"
 echo "Создание кошелька"
--- heminetwork/update.sh
+++ heminetwork/update.sh
@@ -13,7 +13,8 @@ systemctl stop hemi
 wget https://github.com/hemilabs/heminetwork/releases/download/v0.4.4/heminetwork_v0.4.4_linux_amd64.tar.gz
 
 tar -xvf heminetwork_v0.4.4_linux_amd64.tar.gz && rm heminetwork_v0.4.4_linux_amd64.tar.gz
-mv $HOME/heminetwork_v0.4.4_linux_amd64 $HOME/heminetwork
+mv -f $HOME/heminetwork_v0.4.4_linux_amd64/* $HOME/heminetwork
+rm -rf $HOME/heminetwork_v0.4.4_linux_amd64
 
 sudo systemctl daemon-reload
 sudo systemctl start hemi
