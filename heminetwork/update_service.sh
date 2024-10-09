bb260668b181327bc18d15bb548d91a9e414318a
ivan.pohosov <pogosovivan@gmail.com>
2024-10-09 13:43:59 +0300

    update service

--- heminetwork/update_service.sh
+++ heminetwork/update_service.sh
@@ -9,30 +9,9 @@ echo "Обновление сервисного файла"
 echo "-----------------------------------------------------------------------------"
 
 sudo systemctl stop hemi
-sudo systemctl disable hemi &>/dev/null
-rm -rf /etc/systemd/system/hemi.service
 
-sudo tee /etc/systemd/system/hemi.service > /dev/null <<EOF
-[Unit]
-Description=Hemi miner
-After=network.target
+sed -i "s|FEE=50|FEE=250|" /etc/systemd/system/hemi.service
 
-[Service]
-User=$USER
-Environment="POPM_BTC_PRIVKEY=$PRIVATE_KEY"
-Environment="POPM_STATIC_FEE=250"
-Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
-WorkingDirectory=$HOME/heminetwork
-ExecStart=$HOME/heminetwork/popmd
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl enable hemi &>/dev/null
 sudo systemctl daemon-reload
 sudo systemctl start hemi
 
