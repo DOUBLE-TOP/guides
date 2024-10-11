a5c1a1060cd51592007cffedb91394891d9223c8
muller <mdb.crypto2021@gmail.com>
2024-10-11 12:08:51 +0300

    gemini-3h-2024-oct-10

--- subspace/fix_old_proc.sh
+++ subspace/fix_old_proc.sh
@@ -5,8 +5,8 @@ FILE="$HOME/subspace_docker/docker-compose.yml"
 
 # Проверяем, существует ли файл
 if [ -f "$FILE" ]; then
-    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/autonomys\/node:gemini-3h-2024-oct-03/g' $HOME/subspace_docker/docker-compose.yml
-    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/autonomys\/farmer:gemini-3h-2024-oct-03/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/node:.*/ghcr.io\/autonomys\/node:gemini-3h-2024-oct-10/g' $HOME/subspace_docker/docker-compose.yml
+    sed -i 's/ghcr.io\/subspace\/farmer:.*/ghcr.io\/autonomys\/farmer:gemini-3h-2024-oct-10/g' $HOME/subspace_docker/docker-compose.yml
     sed -i 's/--state-pruning", "archive-canonical"/--state-pruning", "140000"/g' $HOME/subspace_docker*/docker-compose.yml
     echo "Обновления были применены."
 else
@@ -15,17 +15,17 @@ fi
 
 docker-compose -f $FILE down
 
-docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-oct-03
-docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-oct-03
+docker rmi -f ghcr.io/subspace/node:gemini-3h-2024-oct-10
+docker rmi -f ghcr.io/subspace/farmer:gemini-3h-2024-oct-10
 docker image prune -a -f
 
 cd $HOME
 rm -rf $HOME/subspace
 git clone https://github.com/subspace/subspace
 cd $HOME/subspace
-git checkout b0e738e7d55a69f40069a9415de49f9a0fc1f67e
+git checkout 5e21f1bf82284c8da4ed0b0c1edfaa0b9431f411
 
-docker build -t ghcr.io/subspace/node:gemini-3h-2024-oct-03 -f Dockerfile-node .
-docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-oct-03 -f Dockerfile-farmer .
+docker build -t ghcr.io/subspace/node:gemini-3h-2024-oct-10 -f Dockerfile-node .
+docker build -t ghcr.io/subspace/farmer:gemini-3h-2024-oct-10 -f Dockerfile-farmer .
 
 docker-compose -f $FILE up -d
