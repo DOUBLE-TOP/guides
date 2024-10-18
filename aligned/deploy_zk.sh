fe9d1d02192cae8a26aa8d53b8ce4b3370e72d91
ivan.pohosov <pogosovivan@gmail.com>
2024-10-18 09:49:50 +0300

    add aligned zk proof deployment

---
+++ aligned/deploy_zk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем зависимости"
+echo "-----------------------------------------------------------------------------"
+
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/foundry.sh | bash &>/dev/null
+
+echo "-----------------------------------------------------------------------------"
+echo "Качаем проект и импортируем кошелек по приватному ключу"
+echo "-----------------------------------------------------------------------------"
+
+cd $HOME
+git clone https://github.com/yetanotherco/aligned_layer.git && cd aligned_layer
+cast wallet import --interactive wallet
+
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем дополнительные зависимости и проходим квиз. Ответы(Nakamoto, Pacific, Green)."
+echo "-----------------------------------------------------------------------------"
+
+cd $HOME/aligned_layer/examples/zkquiz
+make answer_quiz KEYSTORE_PATH=$HOME/.foundry/keystores/wallet
+
+cd $HOME
+rm -rf $HOME/aligned_layer
+rm -rf $HOME/.foundry/keystores/wallet
+
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
