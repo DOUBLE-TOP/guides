cb6f3ee3141fc32d7b1617f10b4fc7dc0aad6910
ivan.pohosov <pogosovivan@gmail.com>
2024-12-07 11:55:35 +0200

    Fix worker2 allora

--- allora/install_worker2.sh
+++ allora/install_worker2.sh
@@ -38,7 +38,7 @@ chmod +x init.config
 ./init.config
 
 sed -i "s|\"8000:8000\"|\"18000:8000\"|" $HOME/allora-huggingface-walkthrough/docker-compose.yaml
-sed -i "s|alloranetwork/allora-offchain-node:.*|alloranetwork/allora-offchain-node:v0.7.0|" $HOME/basic-coin-prediction-node/docker-compose.yaml
+sed -i "s|alloranetwork/allora-offchain-node:.*|alloranetwork/allora-offchain-node:v0.7.0|" $HOME/allora-huggingface-walkthrough/docker-compose.yaml
 
 docker compose up -d --build
 
