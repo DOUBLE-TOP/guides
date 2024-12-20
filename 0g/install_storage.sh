662d56be8bcf8caeb0bf40d8663aa943266e0940
muller <mdb.crypto2021@gmail.com>
2024-12-20 14:00:39 +0200

    fix

--- 0g/install_storage.sh
+++ 0g/install_storage.sh
@@ -8,7 +8,7 @@ install_0gchaind() {
     source $HOME/.profile
     git clone https://github.com/0glabs/0g-chain.git
     cd 0g-chain
-    git checkout v0.2.5
+    git checkout v0.4.0
     make install
     0gchaind version
 }
