1756821fadc5a690900dd5a0ba9b1f740c05a365
ivan.pohosov <pogosovivan@gmail.com>
2024-12-20 13:56:09 +0200

    update shardeum

--- shardeum/health.sh
+++ shardeum/health.sh
@@ -2,8 +2,13 @@
 #thanks for https://raw.githubusercontent.com/ipohosov/public-node-scripts/main/shardeum/shardeum_healthcheck.sh
 
 
-function get_status() {
+function get_node_status() {
     STATUS=$(docker exec -it shardeum-validator operator-cli status | grep state | awk -F': ' '{print $2}')
+    echo "${STATUS}"
+}
+
+function get_gui_status() {
+    STATUS=$(docker exec -it shardeum-validator operator-cli gui status | grep status | awk -F': ' '{print $2}')
     echo "${STATUS}"
 }
 
@@ -12,20 +17,29 @@ cd "$HOME" || exit
 while true
 do
     printf "Check shardeum node status \n"
-    NODE_STATUS=$(get_status)
+    NODE_STATUS=$(get_node_status)
+    GUI_STATUS=$(get_gui_status)
     printf "Current status: ${NODE_STATUS}\n"
     sleep 5s
     if [ -z "$NODE_STATUS" ]; then
         echo "Shardeum нода не запущена"
         docker start shardeum-validator
-        sleep 15m
+        sleep 5m
     else
-        if [[ "${NODE_STATUS}" == *"standby"* ]]; then
-            echo "Status is standby"
-        else
-            echo "Status is not standby"
+        if [[ "${NODE_STATUS}" == *"stopped"* ]]; then
+            echo "Status is stopped"
             docker exec -it shardeum-validator operator-cli start
+        else
+            echo "Status is $NODE_STATUS"
         fi
-        sleep 15m
     fi
+
+    if [[ "${GUI_STATUS}" == *"online"* ]]; then
+        echo "Status is online"
+    else
+        echo "Status is $GUI_STATUS"
+        docker exec -it shardeum-validator operator-cli gui start
+    fi
+    
+    sleep 15m
 done
