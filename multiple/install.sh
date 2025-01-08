ca72c69cc7ddd08951971f7bd3d3e13aa9248932
ivan.pohosov <pogosovivan@gmail.com>
2025-01-08 22:32:21 +0200

    add multiple installer

---
+++ multiple/install.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/docker.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/go.sh | bash &>/dev/null
+
+echo "Установка проекта"
+
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
+    CLIENT_URL="https://cdn.app.multiple.cc/client/linux/x64/multipleforlinux.tar"
+elif [[ "$ARCH" == "aarch64" ]]; then
+    CLIENT_URL="https://cdn.app.multiple.cc/client/linux/arm64/multipleforlinux.tar"
+else
+    echo -e "Неподдерживаемая архитектура системы: $ARCH"
+    exit 1
+fi
+
+wget $CLIENT_URL -O multipleforlinux.tar
+
+echo -e "Распаковка файлов..."
+tar -xvf multipleforlinux.tar
+rm -rf multipleforlinux.tar
+
+cd multipleforlinux
+chmod +x ./multiple-cli
+chmod +x ./multiple-node
+
+echo "PATH=\$PATH:$(pwd)" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+sudo tee /etc/systemd/system/multiple.service > /dev/null << EOF
+[Unit]
+Description=Multiple Network node client on a Linux Operating System
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$HOME/multipleforlinux/multiple-node
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable multiple
+sudo systemctl start multiple
+
+echo "-----------------------------------------------------------------------"
+echo "Привязка аккаунта"
+echo "-----------------------------------------------------------------------"
+
+echo -e "Введите ваш Account ID:"
+read IDENTIFIER
+echo -e "Установите ваш PIN:"
+read PIN
+
+./multiple-cli bind --bandwidth-download 100 --identifier $IDENTIFIER --pin $PIN --storage 200 --bandwidth-upload 100
+
+echo "-----------------------------------------------------------------------"
+echo -e "Команда для проверки статуса ноды:"
+echo "$HOME/multipleforlinux/multiple-cli status"
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
