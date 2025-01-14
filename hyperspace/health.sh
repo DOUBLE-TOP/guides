ce784053cf58b9caf831546f59419debbc879777
ivan.pohosov <pogosovivan@gmail.com>
2025-01-14 08:27:54 +0200

    add hyper health

---
+++ hyperspace/health.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true
+do
+    printf "Check hyperspace logs \n"
+    
+    logs=$(journalctl -n 100 -f -u aios -o cat)
+
+    # Search the logs for the specific pattern and save the result
+    search_result=$(echo "$logs" | grep "Last pong received.*Sending reconnect signal..")
+
+    # Use the search result in an if statement
+    if [ -n "$search_result" ]; then
+        echo "The node is not connected. Restarting the application!"
+        # Restart the application service
+        systemctl restart aios
+    else
+        echo "The pattern for restart was not found. Everything seems fine."
+    fi
+
+    sleep 15m
+done
