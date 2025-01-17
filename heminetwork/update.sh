ec364900690b9942eb8890630ea0808974f6e039
ivan.pohosov <pogosovivan@gmail.com>
2025-01-17 09:24:15 +0200

    Hemi 0.11.1

--- heminetwork/update.sh
+++ heminetwork/update.sh
@@ -15,11 +15,11 @@ systemctl stop hemi
 
 cd $HOME
 
-wget https://github.com/hemilabs/heminetwork/releases/download/v0.11.0/heminetwork_v0.11.0_linux_amd64.tar.gz
+wget https://github.com/hemilabs/heminetwork/releases/download/v0.11.1/heminetwork_v0.11.1_linux_amd64.tar.gz
 
-tar -xvf heminetwork_v0.11.0_linux_amd64.tar.gz && rm heminetwork_v0.11.0_linux_amd64.tar.gz
-mv -f $HOME/heminetwork_v0.11.0_linux_amd64/* $HOME/heminetwork
-rm -rf $HOME/heminetwork_v0.11.0_linux_amd64
+tar -xvf heminetwork_v0.11.1_linux_amd64.tar.gz && rm heminetwork_v0.11.1_linux_amd64.tar.gz
+mv -f $HOME/heminetwork_v0.11.1_linux_amd64/* $HOME/heminetwork
+rm -rf $HOME/heminetwork_v0.11.1_linux_amd64
 
 sudo systemctl daemon-reload
 sudo systemctl start hemi
