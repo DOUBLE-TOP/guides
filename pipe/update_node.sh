778e2aa1efd1b33fc9b6988a2ad3234be7e42e4e
ivan.pohosov <pogosovivan@gmail.com>
2025-01-30 23:24:20 +0200

    fix pipe script

--- pipe/update_node.sh
+++ pipe/update_node.sh
@@ -8,8 +8,8 @@ SERVICE_FILE=/etc/systemd/system/pop.service
 exec_line=$(grep '^ExecStart=' "$SERVICE_FILE")
 
 # Use parameter expansion to extract ram and max-disk values
-ram_value=$(echo "$exec_line" | grep -oP '--ram=\K\d+')
-max_disk_value=$(echo "$exec_line" | grep -oP '--max-disk \K\d+')
+ram_value=$(echo "$exec_line" | sed -n 's/.*--ram=\([0-9]*\).*/\1/p')
+max_disk_value=$(echo "$exec_line" | sed -n 's/.*--max-disk \([0-9]*\).*/\1/p')
 
 echo "Введите ОЗУ. Если хотите оставить текущую($ram_value ГБ) нажмите ENTER: "
 read MEM
