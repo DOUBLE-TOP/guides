f33960304594257584afe1e50bbc4d612c4bad43
ivan.pohosov <pogosovivan@gmail.com>
2025-02-13 01:47:40 +0200

    Fix pipe

--- pipe/update.sh
+++ pipe/update.sh
@@ -10,13 +10,37 @@ echo "--------------------------------------------------------------------------
 
 sudo systemctl stop pop
 
+rm -rf /etc/systemd/system/pop.service
+
+sudo tee /etc/systemd/system/pop.service > /dev/null << EOF
+[Unit]
+Description=Pipe POP Node Service
+After=network.target
+Wants=network-online.target
+
+[Service]
+ExecStart=cd $HOME/opt/dcdn && ./pop --ram=4 --pubKey $PUB_KEY --max-disk 100 --cache-dir $HOME/opt/dcdn/download_cache
+Restart=always
+RestartSec=5
+LimitNOFILE=65536
+LimitNPROC=4096
+StandardOutput=journal
+StandardError=journal
+SyslogIdentifier=dcdn-node
+WorkingDirectory=$HOME/opt/dcdn
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sudo wget -O $HOME/opt/dcdn/pop "https://dl.pipecdn.app/v0.2.5/pop"
 
 chmod +x $HOME/opt/dcdn/pop
 sudo ln -s $HOME/opt/dcdn/pop /usr/local/bin/pop -f
 
-$HOME/opt/dcdn/pop --refresh
+cd $HOME/opt/dcdn && ./pop --refresh
 
+sudo systemctl daemon-reload
 sudo systemctl start pop
 
 echo "-----------------------------------------------------------------------------"
