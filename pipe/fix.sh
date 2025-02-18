c5a1d20d8ebd8b712d256e09b28d7d537f9ff08f
ivan.pohosov <pogosovivan@gmail.com>
2025-02-18 10:57:13 +0200

    update fix

--- pipe/fix.sh
+++ pipe/fix.sh
@@ -14,33 +14,11 @@ sed -i 's|- 80:80|- 1989:80|' $HOME/nwaku-compose/docker-compose.yml
 docker compose up -d
 
 systemctl stop pop
-rm -rf /etc/systemd/system/pop.service
-sudo tee /etc/systemd/system/pop.service > /dev/null << EOF
-[Unit]
-Description=Pipe POP Node Service
-After=network.target
-Wants=network-online.target
-
-[Service]
-ExecStart=$HOME/opt/dcdn/pop --ram=4 --pubKey $PUB_KEY --max-disk 100 --cache-dir $HOME/opt/dcdn/download_cache
-Restart=always
-RestartSec=5
-LimitNOFILE=65536
-LimitNPROC=4096
-StandardOutput=journal
-StandardError=journal
-SyslogIdentifier=dcdn-node
-WorkingDirectory=$HOME/opt/dcdn
-AmbientCapabilities=CAP_NET_BIND_SERVICE CapabilityBoundingSet=CAP_NET_BIND_SERVICE
-
-[Install]
-WantedBy=multi-user.target
-EOF
+sed -i '/^WorkingDirectory=/a AmbientCapabilities=CAP_NET_BIND_SERVICE\nCapabilityBoundingSet=CAP_NET_BIND_SERVICE' /etc/systemd/system/pop.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable pop
 sudo systemctl start pop
-
 
 echo "-----------------------------------------------------------------------------"
 echo "Wish lifechange case with DOUBLETOP"
