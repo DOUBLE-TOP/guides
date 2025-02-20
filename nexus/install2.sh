e49adecf23818bf46f5268ee4121544a7b7ea3f1
ivan.pohosov <pogosovivan@gmail.com>
2025-02-20 16:17:29 +0200

    Update installer

--- nexus/install2.sh
+++ nexus/install2.sh
@@ -6,39 +6,49 @@ echo "--------------------------------------------------------------------------
 
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# remove previous data
-systemctl stop nexus
-systemctl disable nexus
-mkdir -p $HOME/nexus_backups/testnet0
-cp $HOME/.nexus/prover-id $HOME/nexus_backups/testnet0/prover-id
-rm -rf $HOME/.nexus
-sudo rm -rf /etc/systemd/system/nexus.service 
-
+rustup target add riscv32i-unknown-none-elf
 sudo apt install -y protobuf-compiler
 
+# remove previous data
+systemctl stop nexus &>/dev/null
+systemctl disable nexus &>/dev/null
+rm -rf $HOME/.nexus /etc/systemd/system/nexus.service 
 source .profile
 
-NEXUS_HOME=$HOME/.nexus
+NEXUS_HOME="$HOME/.nexus"
+mkdir -p "$NEXUS_HOME"
 
-mkdir -p $NEXUS_HOME
-
-REPO_PATH=$NEXUS_HOME/network-api
+REPO_PATH="$NEXUS_HOME/network-api"
 if [ -d "$REPO_PATH" ]; then
-  echo "$REPO_PATH exists. Updating.";
-  (cd $REPO_PATH && git stash save && git fetch --tags)
+  echo "$REPO_PATH exists. Updating."
+  (
+    cd "$REPO_PATH" || exit
+    git stash
+    git fetch --tags
+  )
 else
-  mkdir -p $NEXUS_HOME
-  (cd $NEXUS_HOME && git clone https://github.com/nexus-xyz/network-api)
+  (
+    cd "$NEXUS_HOME" || exit
+    git clone https://github.com/nexus-xyz/network-api
+  )
 fi
-(cd $REPO_PATH && git -c advice.detachedHead=false checkout $(git rev-list --tags --max-count=1))
 
-cd $REPO_PATH/clients/cli
+(
+  cd "$REPO_PATH" || exit
+  git -c advice.detachedHead=false checkout "$(git rev-list --tags --max-count=1)"
+)
+
+cd "$REPO_PATH/clients/cli" 
 cargo clean
-cargo run --release -- --start --beta
+RUST_BACKTRACE=1 cargo build --release
 
-cp /root/.nexus/network-api/clients/cli/target/release/prover /root/.nexus/network-api/clients/cli/prover
+read -p "Введите node id: " Node_ID
+
+cat $Node_ID > "$NEXUS_HOME"/node_id
+
+cp /root/.nexus/network-api/clients/cli/target/release/nexus-network /root/.nexus/network-api/clients/cli/nexus-network
 
 cat <<EOF | sudo tee /etc/systemd/system/nexus.service >/dev/null
 [Unit]
@@ -53,7 +63,7 @@ RestartSec=30
 LimitNOFILE=65535
 Type=simple
 WorkingDirectory=/root/.nexus/network-api/clients/cli
-ExecStart=/root/.nexus/network-api/clients/cli/prover
+ExecStart=/root/.nexus/network-api/clients/cli/nexus-network --start --env beta
 Restart=on-failure
 
 [Install]
