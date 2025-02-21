b3eee34cae5cb961e006dfbb748db2b40e4a2a1a
muller <mdb.crypto2021@gmail.com>
2025-02-21 13:09:24 +0200

    upd 0.5.6

--- nexus/update.sh
+++ nexus/update.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-
-echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
-echo "-----------------------------------------------------------------------------"
-
-systemctl stop nexus
+source $HOME/.cargo/env
+source $HOME/.bashrc
 
 cd $HOME/.nexus/network-api
-
-git fetch
+git stash
+git fetch --tags
 git -c advice.detachedHead=false checkout $(git rev-list --tags --max-count=1)
 
-cd $HOME/.nexus/network-api/clients/cli
-cargo build --release --bin prover
-
-rm -rf $HOME/.nexus/network-api/clients/cli/prover
-cp $HOME/.nexus/network-api/clients/cli/target/release/prover $HOME/.nexus/network-api/clients/cli/prover
-
-systemctl start nexus
+cd $HOME/.nexus/ && ./nexus-network start --env beta
 
 echo "-----------------------------------------------------------------------------"
 echo "Wish lifechange case with DOUBLETOP"
