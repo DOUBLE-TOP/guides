4e30d5d799b0f0d80a94fd25d37fe2d192950344
Oleg <legolassoi@gmail.com>
2025-03-20 21:55:34 +0200

    Update install.sh
    updated GRPC_URL

--- layeredge/install.sh
+++ layeredge/install.sh
@@ -67,7 +67,7 @@ git clone https://github.com/Layer-Edge/light-node.git &>/dev/null
 cd light-node
 
 cat > .env <<EOL
-GRPC_URL=34.31.74.109:9090
+GRPC_URL=grpc.testnet.layeredge.io:9090
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
 API_REQUEST_TIMEOUT=100
