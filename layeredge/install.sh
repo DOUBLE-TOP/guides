163c7041a21c4beedbc02d4450c3565e271dfdf4
Oleg <legolassoi@gmail.com>
2025-03-20 22:53:26 +0200

    Update install.sh
    fix prover URL

--- layeredge/install.sh
+++ layeredge/install.sh
@@ -69,7 +69,7 @@ cd light-node
 cat > .env <<EOL
 GRPC_URL=grpc.testnet.layeredge.io:9090
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
-ZK_PROVER_URL=https://layeredge.mintair.xyz/
+ZK_PROVER_URL=https://layeredge.mintair.xyz
 API_REQUEST_TIMEOUT=100
 POINTS_API=https://light-node.layeredge.io
 PRIVATE_KEY='${private_key_value}'
