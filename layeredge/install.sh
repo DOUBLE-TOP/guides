37c8963dcbd10b5b8288c506ac9bc56bf96e5a2b
Oleg <legolassoi@gmail.com>
2025-03-20 22:24:44 +0200

    Update install.sh
    newest POINTS_API endpoint url from official git

--- layeredge/install.sh
+++ layeredge/install.sh
@@ -71,7 +71,7 @@ GRPC_URL=grpc.testnet.layeredge.io:9090
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
 API_REQUEST_TIMEOUT=100
-POINTS_API=http://127.0.0.1:8080
+POINTS_API=https://light-node.layeredge.io
 PRIVATE_KEY='${private_key_value}'
 EOL
 
