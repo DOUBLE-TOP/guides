ac0bce3a26920cf39fab3da7a416ea5057cd65ce
muller <mdb.crypto2021@gmail.com>
2025-03-31 19:26:47 +0300

    add fix

--- soundness/install.sh
+++ soundness/install.sh
@@ -8,14 +8,13 @@ curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
-
+source ~/.profile
 echo "-----------------------------------------------------------------------------"
 echo "Установка CLI"
 echo "-----------------------------------------------------------------------------"
 curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
 sleep 1
 source ~/.bashrc
-source ~/.profile
 sleep 3
 soundnessup install
 echo "Soundnessup CLI установлен"
