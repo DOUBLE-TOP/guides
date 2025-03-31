84b0e642338115415d9701c1eaa0dbeedf4fbf1e
muller <mdb.crypto2021@gmail.com>
2025-03-31 18:55:49 +0300

    install cli soundnessup

---
+++ soundness/install.sh
@@ -0,0 +1,29 @@
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+echo "Устанавливаем софт (временной диапазон ожидания ~5-30 min.)"
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
+echo "Весь необходимый софт установлен"
+echo "-----------------------------------------------------------------------------"
+
+echo "-----------------------------------------------------------------------------"
+echo "Установка CLI"
+echo "-----------------------------------------------------------------------------"
+curl -sSL https://raw.githubusercontent.com/soundnesslabs/soundness-layer/main/soundnessup/install | bash
+source ~/.bashrc
+sleep 1
+soundnessup install
+echo "Soundnessup CLI установлен"
+
+echo "Генерирую пару ключей | В процессе Вас попросят ввести пароль. Запомните его"
+sleep 3
+soundness-cli generate-key --name my-key
+sleep 3
+echo "Ключи сгенерированы, сохраните мнемоническую фразу"
+
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
