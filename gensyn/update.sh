c5bc8e92dd0968ce865691fd5c8e531394657265
muller <mdb.crypto2021@gmail.com>
2025-04-10 14:13:37 +0300

    add --rebase to git pull

--- gensyn/update.sh
+++ gensyn/update.sh
@@ -4,6 +4,6 @@ curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | b
 echo "-----------------------------------------------------------------------------"
 sudo systemctl stop gensyn.service
 cd $HOME/rl-swarm
-git pull
+git pull --rebase
 sudo systemctl start gensyn.service
 echo "Обновлено успешно"
