121839be5a7204acdf6a4a805cf1a02f6c40fa7b
Oleg <legolassoi@gmail.com>
2025-04-10 16:10:46 +0300

    Update update.sh

--- gensyn/update.sh
+++ gensyn/update.sh
@@ -4,6 +4,7 @@ curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | b
 echo "-----------------------------------------------------------------------------"
 sudo systemctl stop gensyn.service
 cd $HOME/rl-swarm
+git reset --hard HEAD
 git pull --rebase
 sudo systemctl start gensyn.service
 echo "Обновлено успешно"
