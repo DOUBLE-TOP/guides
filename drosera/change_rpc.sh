74baa5715fcada22f4949b39a71e6985c57c29bc
Oleg <legolassoi@gmail.com>
2025-04-17 16:55:49 +0300

    Create change_rpc.sh

---
+++ drosera/change_rpc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+config_file=~/drosera/drosera.toml
+service_file="/etc/systemd/system/drosera.service"
+
+read -p "Введите новый RPC адрес: " new_rpc
+if [ -f "$config_file" ]; then
+    sed -i "s|^drosera_rpc = \".*\"|drosera_rpc = \"$new_rpc\"|" "$config_file"
+    echo "RPC изменен в файле $config_file"
+else
+    echo "Файл $config_file не найден"
+    exit 1
+fi
+
+if [ ! -f "$service_file" ]; then
+    echo "Файл сервиса не найден: $service_file"
+    exit 1
+fi
+
+sed -i -E 's|--eth-rpc-url [^ ]+|--eth-rpc-url '"$new_rpc"'|g' "$service_file"
+
+echo "Изменили --eth-rpc-url в файле $service_file"
+
+
+
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl restart drosera.service
+echo "Сервис перезапущен."
