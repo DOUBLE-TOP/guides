893c2c94522daf0ed7bf317efd71845641e8b782
Oleg <legolassoi@gmail.com>
2025-05-08 21:04:44 +0300

    Create update.sh

---
+++ drosera/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+
+echo "Останавливаем drosera сервис"
+service drosera stop
+source /root/.profile
+echo "Обновляемм Drosera CLI"
+curl -s -L https://app.drosera.io/install | bash > /dev/null 2>&1
+droseraup &>/dev/null
+
+if [ ! -f drosera.toml ]; then
+  cd drosera
+fi
+echo "Обновляем значение drosera_rpc в файле drosera.toml"
+sed -i 's|https://seed-node.testnet.drosera.io|https://relay.testnet.drosera.io|g' drosera.toml
+
+# get private key to do drosera apply with it
+SERVICE_FILE="/etc/systemd/system/drosera.service"
+private_key=$(grep 'ExecStart=' "$SERVICE_FILE" | sed -n 's/.*--eth-private-key \([^ ]*\).*/\1/p')
+
+drosera apply --private-key="$private_key"
+
+echo "Стартуем drosera сервис"
+service drosera start
