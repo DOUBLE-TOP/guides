68633db2967461c1d1b4db8ed1be0b279576aa57
Oleg <legolassoi@gmail.com>
2025-06-23 22:19:04 +0300

    Update install2.sh

--- nexus/install2.sh
+++ nexus/install2.sh
@@ -16,28 +16,4 @@ curl https://cli.nexus.xyz/ | sh
 
 source /root/.bashrc
 
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/refs/heads/main/glibc239.sh | bash &>/dev/null
-
-
-echo "Установка Nexus завершена. Продолжайте по гайду"
-root@vesta-yyu4hj:~# nano nexus.sh
-root@vesta-yyu4hj:~# cat nexus.sh
-#!/bin/bash
-
-echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
-echo "-----------------------------------------------------------------------------"
-
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup target add riscv32i-unknown-none-elf
-
-sudo apt install -y protobuf-compiler
-
-curl https://cli.nexus.xyz/ | sh
-
-source /root/.bashrc
-
 echo "Установка Nexus завершена. Продолжайте по гайду"
