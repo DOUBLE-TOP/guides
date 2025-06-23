410c6e80227ffbaf862d58996d5d019082286965
Oleg <legolassoi@gmail.com>
2025-06-23 22:18:43 +0300

    Update install2.sh

--- nexus/install2.sh
+++ nexus/install2.sh
@@ -14,75 +14,30 @@ sudo apt install -y protobuf-compiler
 
 curl https://cli.nexus.xyz/ | sh
 
-source .profile
+source /root/.bashrc
 
-SERVICE_NAME="nexus.service"
-SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME"
-LOG_FILE="/var/log/nexus.log"
-LOGROTATE_CONF="/etc/logrotate.d/nexus"
-HOME_DIR="$HOME"
-read -p "Введите NODE ID: " NODE_ID
-
-# удаляем сервис если уже стоит
-if systemctl list-units --type=service --all | grep -q "$SERVICE_NAME"; then
-    sudo systemctl stop "$SERVICE_NAME"
-    sudo systemctl disable "$SERVICE_NAME"
-    if [ -f "$SERVICE_FILE" ]; then
-        sudo rm "$SERVICE_FILE"
-    fi
-    > "$LOG_FILE"
-    sudo systemctl daemon-reload
-    echo "Существующий $SERVICE_NAME удален."
-fi
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/refs/heads/main/glibc239.sh | bash &>/dev/null
 
 
-# Создаем systemd сервис
-cat <<EOF | sudo tee "$SERVICE_FILE" > /dev/null
-[Unit]
-Description=Nexus Service
-After=network.target
+echo "Установка Nexus завершена. Продолжайте по гайду"
+root@vesta-yyu4hj:~# nano nexus.sh
+root@vesta-yyu4hj:~# cat nexus.sh
+#!/bin/bash
 
-[Service]
-User=root
-WorkingDirectory=$HOME_DIR
-ExecStart=/bin/bash -c 'source /root/.bashrc && nexus-network start --node-id $NODE_ID'
-Restart=always
-RestartSec=5
-StandardOutput=append:$LOG_FILE
-StandardError=append:$LOG_FILE
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
 
-[Install]
-WantedBy=multi-user.target
-EOF
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup target add riscv32i-unknown-none-elf
 
-sudo systemctl daemon-reload
-sudo systemctl enable nexus.service
-sudo systemctl start "$SERVICE_NAME"
+sudo apt install -y protobuf-compiler
 
-# Настраиваем Logrotate
-if [ ! -f "$LOG_FILE" ]; then
-    touch "$LOG_FILE"
-    chmod 644 "$LOG_FILE"
-fi
-if [ -f "$LOGROTATE_CONF" ]; then
-    echo "Logrotate уже настроен."
-else
-    # Create logrotate config
-    sudo tee "$LOGROTATE_CONF" > /dev/null <<EOF
-$LOG_FILE {
-    daily
-    rotate 2
-    compress
-    delaycompress
-    missingok
-    notifempty
-    copytruncate
-}
-EOF
+curl https://cli.nexus.xyz/ | sh
 
-    # Запускаем вручную  logrotate
-    logrotate -f "$LOGROTATE_CONF" &>/dev/null
-fi
+source /root/.bashrc
 
-echo "Установка Nexus завершена. LogRotate настроен."
-echo "Смотреть логи можно командой: tail -n 20 -f $LOG_FILE"
+echo "Установка Nexus завершена. Продолжайте по гайду"
