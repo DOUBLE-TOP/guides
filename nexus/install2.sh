7b203665c654ee21266468759b371c60fac5c5c0
Oleg <legolassoi@gmail.com>
2025-06-23 18:08:24 +0300

    Update install2.sh

--- nexus/install2.sh
+++ nexus/install2.sh
@@ -4,96 +4,83 @@ echo "--------------------------------------------------------------------------
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/doubletop.sh | bash
 echo "-----------------------------------------------------------------------------"
 
-curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/main.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/ufw.sh | bash &>/dev/null
-
+curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/rust.sh | bash &>/dev/null
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
-
-sudo apt purge -y protobuf-compiler
-
-ARCH=$(uname -m)
-
-if [[ "$ARCH" == "x86_64" ]]; then
-    PROTOC_VERSION="25.2"
-    PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip"
-elif [[ "$ARCH" == "aarch64" ]]; then
-    PROTOC_VERSION="3.19.1"
-    PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-aarch_64.zip"
-else
-    echo "Архитектура $ARCH не поддерживается."
-    exit 1
-fi
-
-curl -LO "$PROTOC_URL"
-unzip "protoc-${PROTOC_VERSION}-linux-*.zip" -d "$HOME/.local"
-# curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip
-# unzip protoc-25.2-linux-x86_64.zip -d $HOME/.local
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
-sleep 2
-export PATH=$HOME/.local/bin:$PATH
-protoc --version
 rustup target add riscv32i-unknown-none-elf
 
-# remove previous data
-systemctl stop nexus &>/dev/null
-systemctl disable nexus &>/dev/null
-rm -rf $HOME/.nexus /etc/systemd/system/nexus.service 
-#source .profile
+sudo apt install -y protobuf-compiler
 
-# swap file fix
-SWAP_LOCATE=$(swapon --show | awk 'NR==2 {print $1}')
-if [[ -n "$SWAP_LOCATE" ]]; then
-    swapoff "$SWAP_LOCATE" && rm -f "$SWAP_LOCATE"
+source .profile
+
+SERVICE_NAME="nexus.service"
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME"
+LOG_FILE="/var/log/nexus.log"
+LOGROTATE_CONF="/etc/logrotate.d/nexus"
+HOME_DIR="$HOME"
+read -p "Введите NODE ID: " NODE_ID
+
+# удаляем сервис если уже стоит
+if systemctl list-units --type=service --all | grep -q "$SERVICE_NAME"; then
+    sudo systemctl stop "$SERVICE_NAME"
+    sudo systemctl disable "$SERVICE_NAME"
+    if [ -f "$SERVICE_FILE" ]; then
+        sudo rm "$SERVICE_FILE"
+    fi
+    > "$LOG_FILE"
+    sudo systemctl daemon-reload
+    echo "Существующий $SERVICE_NAME удален."
 fi
-fallocate -l 6G /swapfile && \
-chmod 600 /swapfile && \
-mkswap /swapfile && \
-swapon /swapfile && \
-swapon --show && \
-echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab && \
-sysctl vm.swappiness=10 && \
-sysctl vm.vfs_cache_pressure=50 && \
-echo "vm.swappiness=10" >> /etc/sysctl.conf && \
-echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
 
 
-NEXUS_HOME="$HOME/.nexus"
-mkdir -p "$NEXUS_HOME"
+# Создаем systemd сервис
+cat <<EOF | sudo tee "$SERVICE_FILE" > /dev/null
+[Unit]
+Description=Nexus Service
+After=network.target
 
-REPO_PATH="$NEXUS_HOME/network-api"
-if [ -d "$REPO_PATH" ]; then
-  echo "$REPO_PATH exists. Updating."
-  (
-    cd "$REPO_PATH" || exit
-    git stash
-    git fetch --tags
-  )
+[Service]
+User=root
+WorkingDirectory=$HOME_DIR
+ExecStart=/bin/bash -c 'source /root/.profile && nexus-network start --node-id $NODE_ID'
+Restart=always
+RestartSec=5
+StandardOutput=append:$LOG_FILE
+StandardError=append:$LOG_FILE
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable nexus.service
+sudo systemctl start "$SERVICE_NAME"
+
+# Настраиваем Logrotate
+if [ ! -f "$LOG_FILE" ]; then
+    touch "$LOG_FILE"
+    chmod 644 "$LOG_FILE"
+fi
+if [ -f "$LOGROTATE_CONF" ]; then
+    echo "Logrotate уже настроен."
 else
-  (
-    cd "$NEXUS_HOME" || exit
-    git clone https://github.com/nexus-xyz/network-api
-  )
+    # Create logrotate config
+    sudo tee "$LOGROTATE_CONF" > /dev/null <<EOF
+$LOG_FILE {
+    daily
+    rotate 2
+    compress
+    delaycompress
+    missingok
+    notifempty
+    copytruncate
+}
+EOF
+
+    # Запускаем вручную  logrotate
+    logrotate -f "$LOGROTATE_CONF" &>/dev/null
 fi
 
-(
-  cd "$REPO_PATH" || exit
-  git -c advice.detachedHead=false checkout "$(git rev-list --tags --max-count=1)"
-)
-
-cd "$REPO_PATH/clients/cli" 
-
-RUST_BACKTRACE=1 cargo build --release
-
-cp /root/.nexus/network-api/clients/cli/target/release/nexus-network /root/.nexus/nexus-network
-
-lsb_release -a
-rustc --version
-cargo --version
-file $HOME/.local/bin/protoc
-
-echo "-----------------------------------------------------------------------------"
-echo "Wish lifechange case with DOUBLETOP"
-echo "-----------------------------------------------------------------------------"
+echo "Установка Nexus завершена. LogRotate настроен."
+echo "Смотреть логи можно командой: tail -n 20 -f $LOG_FILE"
