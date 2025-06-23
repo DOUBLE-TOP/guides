41bb2b5f682d02537d001c02eddb2f7080be9e45
Oleg <legolassoi@gmail.com>
2025-06-23 23:18:39 +0300

    Update install2.sh

--- nexus/install2.sh
+++ nexus/install2.sh
@@ -16,4 +16,19 @@ curl https://cli.nexus.xyz/ | sh
 
 source /root/.bashrc
 
+# Get the major.minor version number of glibc from ldd
+version_str=$(ldd --version | head -n1)
+# Extract version number like "2.35" or "2.39"
+version_num=$(echo "$version_str" | grep -oP '\d+\.\d+' | head -n1)
+version_lt() {
+    # Use sort -V (version sort)
+    [ "$(printf '%s\n%s' "$1" "$2" | sort -V | head -n1)" != "$2" ]
+}
+REQUIRED_VERSION="2.39"
+
+if version_lt "$version_num" "$REQUIRED_VERSION"; then
+    echo "Ставим $REQUIRED_VERSION. Ориентировочное время 10-20 минут."
+    curl -fsSL https://raw.githubusercontent.com/DOUBLE-TOP/tools/refs/heads/main/glibc239.sh | bash &>/dev/null
+fi
+
 echo "Установка Nexus завершена. Продолжайте по гайду"
