3f4ceb41d27493d56150e9d0897372159ed0efa3
Oleg <legolassoi@gmail.com>
2025-06-26 10:48:44 +0300

    Update update.sh

--- nexus/update.sh
+++ nexus/update.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-source $HOME/.cargo/env
+source /root/.bashrc
 
-cd $HOME/.nexus/network-api
-git stash
-git fetch --tags
-git -c advice.detachedHead=false checkout $(git rev-list --tags --max-count=1)
-
-cd $HOME/.nexus/network-api/clients/cli && cargo run -r -- start --env beta
+yes | curl -fsSL https://cli.nexus.xyz/ | sh
 
 echo "-----------------------------------------------------------------------------"
 echo "Wish lifechange case with DOUBLETOP"
