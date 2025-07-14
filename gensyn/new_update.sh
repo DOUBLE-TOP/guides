527c3e62e115f5a5845666a550c720fe5b478d9f
Oleg <legolassoi@gmail.com>
2025-07-14 14:55:46 +0300

    Update new_update.sh

--- gensyn/new_update.sh
+++ gensyn/new_update.sh
@@ -216,7 +216,7 @@ if [ "$CONNECT_TO_TESTNET" = true ]; then
 
     # Docker image already builds it, no need to again.
     if [ -z "$DOCKER" ]; then
-        yarn install --immutable --silent
+        yarn install --immutable --silent > /dev/null 2>&1
         echo "Building server"
         yarn build > "$ROOT/logs/yarn.log" 2>&1
     fi
