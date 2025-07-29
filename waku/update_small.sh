9a47323924f933febe15e2831a930b050d379ad2
Oleg <legolassoi@gmail.com>
2025-07-29 10:30:06 +0300

    Update update_small.sh

--- waku/update_small.sh
+++ waku/update_small.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker-compose -f $HOME/nwaku-compose/docker-compose.yml down
+
 cd $HOME/nwaku-compose/
 # убираем изменения в docker-compose для коректного git pull
 git checkout -- docker-compose.yml
@@ -12,4 +14,7 @@ sed -i 's/127\.0\.0\.1:4000:4000/0.0.0.0:4044:4000/g' $HOME/nwaku-compose/docker
 sed -i 's|127.0.0.1:8003:8003|127.0.0.1:8333:8003|' $HOME/nwaku-compose/docker-compose.yml
 sed -i 's/:5432:5432/:5444:5432/g' $HOME/nwaku-compose/docker-compose.yml
 sed -i 's/80:80/8081:80/g' $HOME/nwaku-compose/docker-compose.yml
+
+docker compose -f $HOME/nwaku-compose/docker-compose.yml up -d
+
 echo "Готово."
